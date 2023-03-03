fb0d0c6dd20c9e8baac881ff808b74e6de9af84b
LimTaeHun <lsw463@naver.com>
2023-03-04 07:12:32 +0900

    Add: Interface Folder, IObserver.cs

---
+++ Assets/01.Scripts/Interface/IObserver.cs
@@ -0,0 +1,12 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LTH.ColorMatch.Interfaces
+{
+    public interface IObserver
+    {
+        public void Update();
+    }
+}
