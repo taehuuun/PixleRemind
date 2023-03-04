0c6ef6d3a6e233859be3b26c95a0cf90af2a2d31
LimTaeHun <lsw463@naver.com>
2023-03-05 00:40:00 +0900

    Modify: CustomColor 클래스에서 흑백 값도 관리함으로 기존 blackWhtieData 변수 제거 및 관련 코드 수정

--- Assets/01.Scripts/Data/PixelArtData.cs
+++ Assets/01.Scripts/Data/PixelArtData.cs
@@ -9,16 +9,15 @@ namespace LTH.ColorMatch.Data
         public GalleryTopic topic;
         public string title;
         public int size;
+        public int chanceCnt;
         public Difficulty difficulty;
         public ColorData colorData;
-        public ColorData blackWhiteData;
 
-        public PixelArtData(GalleryTopic gt, string t, ColorData c, ColorData b, int s, Difficulty d)
+        public PixelArtData(GalleryTopic gt, string t, ColorData c, int s, Difficulty d)
         {
             topic = gt;
             title = t;
             colorData = c;
-            blackWhiteData = b;
             size = s;
             difficulty = d;
         }
