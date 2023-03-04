162e22d2bd03ef75cec5bd21851855f8c086d51a
LimTaeHun <lsw463@naver.com>
2023-03-04 10:59:23 +0900

    Add: ISubject.cs 추가

---
+++ Assets/01.Scripts/Interface/ISubject.cs
@@ -0,0 +1,9 @@
+namespace LTH.ColorMatch.Interfaces
+{
+    public interface ISubject
+    {
+        void RegisterObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers();
+    }
+}
