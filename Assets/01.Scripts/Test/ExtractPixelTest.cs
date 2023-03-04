65df3856b6c18c03f3d1818b2f8ce0004b8a6ed7
LimTaeHun <lsw463@naver.com>
2023-03-05 00:57:24 +0900

    Remove: 테스트 코드중 불필요 코드 제거

--- Assets/01.Scripts/Test/ExtractPixelTest.cs
+++ Assets/01.Scripts/Test/ExtractPixelTest.cs
@@ -1,14 +1,9 @@
-using System.Collections.Generic;
-using System.Drawing;
-using System.Security.Cryptography;
 using LTH.ColorMatch.Data;
 using LTH.ColorMatch.Enums;
 using LTH.ColorMatch.Managers;
 using LTH.ColorMatch.Utill;
 using Newtonsoft.Json;
 using UnityEngine;
-using UnityEngine.UI;
-using Color = UnityEngine.Color;
 
 namespace LTH.ColorMatch.Test
 {
@@ -27,42 +22,13 @@ namespace LTH.ColorMatch.Test
         {
             if (DataManager.JsonFileExist(testImage.name))
             {
-                Debug.Log("Load");
                 testData = DataManager.LoadJsonData<PixelArtData>(testImage.name);
             }
             else
             {
-                Debug.Log("New Gen And Save");
                 testData = PixelDataExtractor.ExportPixelData(GalleryTopic.Animal, testImage.name, testImage,Difficulty.Easy);
                 DataManager.SaveJsonData(JsonConvert.SerializeObject(testData), testImage.name);
             }
-            
-            Debug.Log(testData.size);
-            
-            // if (isBlackWhite)
-            // {
-            //     GenerateBoard(600, testData.size, testData.blackWhiteData.Pixels);
-            // }
-            // else
-            // {
-            //     GenerateBoard(600, testData.size, testData.colorData.Pixels);
-            // }
         }
-        
-        // private void GenerateBoard(int boardSize, int cellSize, List<List<CustomColor>> pixelData)
-        // {
-        //     board = new Cell[cellSize,cellSize];
-        //     int width = boardSize / cellSize;
-        //     int height = boardSize / cellSize;
-        //
-        //     for (int y = 0; y < cellSize; y++)
-        //     {
-        //         for (int x = 0; x < cellSize; x++)
-        //         {
-        //             board[y, x] = Instantiate(cellPrefabs);
-        //             board[y, x].SetCell(cellParent, width, height, x, y, pixelData[y][x].ConvertColor());
-        //         }
-        //     }
-        // }
     }
 }
