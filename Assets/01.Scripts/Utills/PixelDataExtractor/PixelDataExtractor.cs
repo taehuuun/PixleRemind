30145ff9ea6ed7f03ff0964ade0bcef36d148e54
LimTaeHun <lsw463@naver.com>
2023-03-07 13:49:20 +0900

    Modify: PixelArtData클래스 수정에 따른 코드 수정

--- Assets/01.Scripts/Utills/PixelDataExtractor/PixelDataExtractor.cs
+++ Assets/01.Scripts/Utills/PixelDataExtractor/PixelDataExtractor.cs
@@ -2,13 +2,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using LTH.ColorMatch.Data;
 using LTH.ColorMatch.Enums;
+using UnityEngine.UIElements;
 
 namespace LTH.ColorMatch.Utill
 {
     public class PixelDataExtractor : MonoBehaviour
     {
+        private static int maxFillCnt = 0;
         private static ColorData ExtractPixelData(Texture2D extractTarget)
         {
+            maxFillCnt = 0;
             ColorData pixelDatas = new ColorData();
             
             for (int y = 0; y < extractTarget.height; y++)
@@ -18,11 +21,16 @@ namespace LTH.ColorMatch.Utill
                 for (int x = 0; x < extractTarget.width; x++)
                 {
                     Color getPixelColor = extractTarget.GetPixel(x, y);
+
                     CustomColor customColor = new CustomColor(getPixelColor.r,getPixelColor.g,getPixelColor.b,getPixelColor.a);
                     rowPixelDatas.Add(customColor);
+
+                    if (getPixelColor.a >= 1)
+                        maxFillCnt++;
                 }
                 
-                pixelDatas.Pixels.Add(rowPixelDatas);
+                if(rowPixelDatas.Count > 0)                
+                    pixelDatas.Pixels.Add(rowPixelDatas);
             }
             
             pixelDatas.Pixels.Reverse();
@@ -33,7 +41,7 @@ namespace LTH.ColorMatch.Utill
         public static PixelArtData ExportPixelData(GalleryTopic topic, string title, Texture2D pixelArtImg, Difficulty difficulty)
         {
             ColorData colorData = ExtractPixelData(pixelArtImg);
-            PixelArtData newData = new PixelArtData(topic, title, colorData, colorData.Pixels.Count, Difficulty.Easy);
+            PixelArtData newData = new PixelArtData(topic, title, colorData, pixelArtImg.width,0,maxFillCnt, difficulty);
             
             return newData;
         }
