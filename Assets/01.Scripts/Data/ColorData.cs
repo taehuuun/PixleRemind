7eec2509f9851e361a9b0030df524dd8ac6847d6
LimTaeHun <lsw463@naver.com>
2023-03-09 11:46:23 +0900

    Modify: 기존 PixelArtData의 remainPixel변수를 ColorData로 이동

--- Assets/01.Scripts/Data/ColorData.cs
+++ Assets/01.Scripts/Data/ColorData.cs
@@ -4,6 +4,7 @@ namespace LTH.ColorMatch.Data
 {
     public class ColorData
     {
+        public int remainPixel;
         public List<List<CustomColor>> Pixels = new List<List<CustomColor>>();
     }
 }
