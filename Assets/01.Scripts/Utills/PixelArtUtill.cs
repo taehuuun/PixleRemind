c6262452fc4ea9e82c13dd9059528ec33a939406
LimTaeHun <lsw463@naver.com>
2023-03-10 00:25:55 +0900

    Feat: 썸네일 데이터를 추출하는 ExtractThumbnailData 함수 구현 및 ExportPixelData 함수 수정

--- Assets/01.Scripts/Utills/PixelArtUtill.cs
+++ Assets/01.Scripts/Utills/PixelArtUtill.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using LTH.ColorMatch.Data;
 using LTH.ColorMatch.Enums;
@@ -35,10 +36,52 @@ namespace LTH.ColorMatch.Utill
             return pixelDatas;
         }
 
+        public static string ExtractThumbnailData(ColorData colorData, int size)
+        {
+            Texture2D thumbNail = new Texture2D(size, size, TextureFormat.RGBA32, false)
+            {
+                filterMode = FilterMode.Point
+            };
+            
+            for (int y = 0; y < size; y++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    CustomColor customColor = colorData.Pixels[y][x];
+                    ColorMatchColor pixelColor = customColor.complete
+                        ? customColor.originColorMatchColor
+                        : customColor.grayColorMatchColor;
+                    Color setColor = new Color(pixelColor.r, pixelColor.g, pixelColor.b, pixelColor.a);
+                    thumbNail.SetPixel(x, y, setColor);
+                }
+            }
+            
+            thumbNail.Apply();
+            
+            byte[] bytes = thumbNail.EncodeToPNG();
+            string textureData = Convert.ToBase64String(bytes);
+            return textureData;
+        }
+        public static Sprite MakeThumbnail(string thumbNailData, int size)
+        {
+            Texture2D thumbNail = new Texture2D(size, size, TextureFormat.RGBA32, false)
+            {
+                filterMode = FilterMode.Point
+            };
+
+            byte[] bytes = Convert.FromBase64String(thumbNailData);
+
+            thumbNail.LoadImage(bytes);
+            Sprite newSprite = Sprite.Create(thumbNail, new Rect(0, 0, thumbNail.width, thumbNail.height), new Vector2(0.5f,0.5f));
+            
+            return newSprite;
+        }
         public static PixelArtData ExportPixelData(GalleryTopic topic, string title, Texture2D pixelArtImg, Difficulty difficulty)
         {
             ColorData colorData = ExtractPixelData(pixelArtImg);
-            PixelArtData newData = new PixelArtData(topic, title, colorData, pixelArtImg.width,0,false, difficulty);
+            string thumbData = ExtractThumbnailData(colorData, pixelArtImg.width);
+            PixelArtData newData = new PixelArtData(topic, title, thumbData, pixelArtImg.width, 0, false, difficulty,
+                colorData);
             
             return newData;
         }
