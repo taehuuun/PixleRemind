2225dedfa2adfa9735b54c893785d65c804aa3f5
LimTaeHun <lsw463@naver.com>
2023-03-09 12:00:02 +0900

    Modify: PixelDataExtractor.cs 기능 PixelArtUtill.cs로 이전 및 기존 PixelDataExtractor.cs 삭제

--- Assets/01.Scripts/Utills/PixelArtUtill.cs
+++ Assets/01.Scripts/Utills/PixelArtUtill.cs
@@ -1,17 +1,46 @@
+using System.Collections.Generic;
+using LTH.ColorMatch.Data;
+using LTH.ColorMatch.Enums;
+using UnityEngine;
+
 namespace LTH.ColorMatch.Utill
 {
     public class PixelArtUtill
     {
-        // Start is called before the first frame update
-        void Start()
+        private static ColorData ExtractPixelData(Texture2D extractTarget)
         {
+            ColorData pixelDatas = new ColorData();
             
+            for (int y = 0; y < extractTarget.height; y++)
+            {
+                List<CustomColor> rowPixelDatas = new List<CustomColor>();
+                
+                for (int x = 0; x < extractTarget.width; x++)
+                {
+                    Color getPixelColor = extractTarget.GetPixel(x, y);
+
+                    CustomColor customColor = new CustomColor(getPixelColor.r,getPixelColor.g,getPixelColor.b,getPixelColor.a);
+                    rowPixelDatas.Add(customColor);
+
+                    if (getPixelColor.a >= 1)
+                        pixelDatas.remainPixel++;
+                }
+                
+                if(rowPixelDatas.Count > 0)                
+                    pixelDatas.Pixels.Add(rowPixelDatas);
+            }
+            
+            pixelDatas.Pixels.Reverse();
+            
+            return pixelDatas;
         }
 
-        // Update is called once per frame
-        void Update()
+        public static PixelArtData ExportPixelData(GalleryTopic topic, string title, Texture2D pixelArtImg, Difficulty difficulty)
         {
+            ColorData colorData = ExtractPixelData(pixelArtImg);
+            PixelArtData newData = new PixelArtData(topic, title, colorData, pixelArtImg.width,0,false, difficulty);
             
+            return newData;
         }
     }
 }
