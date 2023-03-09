98d490c3a9a55d1717235447b2d0305606aa0aac
LimTaeHun <lsw463@naver.com>
2023-03-10 00:29:32 +0900

    Modify: 기존 주석 설명 제거 및 Save, Load기능을 하는 함수의 매개변수 수정 및 Master Path 추가

--- Assets/01.Scripts/Manager/DataManager.cs
+++ Assets/01.Scripts/Manager/DataManager.cs
@@ -7,56 +7,32 @@ namespace LTH.ColorMatch.Managers
 {
     public class DataManager : MonoBehaviour
     {
-        private static readonly string BasePath = Application.persistentDataPath;
-        
-        /// <summary>
-        /// 주어진 Topic(디렉토리)에서 모든 파일 이름을 가져오는 함수
-        /// </summary>
-        /// <param name="topic">Topic(디렉토리)</param>
-        /// <returns>파일 이름이 담긴 리스트</returns>
+        public static readonly string BasePath = Application.persistentDataPath;
+        public static readonly string GalleryDataPath = Path.Combine(BasePath, "Gallery");
+
         public static List<string> GetPixelArts(string topic)
         {
-            return GetPixelArtsNames(topic);
+            return GetPixelArtNames(topic);
         }
-        /// <summary>
-        /// BasePath의 모들 Topic을 가져오는 함수
-        /// </summary>
-        /// <returns>Topic이 담긴 리스트</returns>
         public static List<string> GetTopics()
         {
             return GetTopicNames();
         }
-        /// <summary>
-        /// JSON 데이터를 파일로 저장하는 함수
-        /// </summary>
-        /// <param name="jsonData">저장할 JSON 데이터</param>
-        /// <param name="fileName">저장할 파일 이름</param>
-        public static void SaveJsonData(string topic,string pixelArt,string jsonData)
+        public static void SaveJsonData(string savePath, string fileName, string jsonData)
         {
-            SaveJsonDataToFile(topic, pixelArt, jsonData);
+            SaveJsonDataToFile(savePath, fileName, jsonData);
         }
-        /// <summary>
-        /// JSON 데이터를 로드하는 함수
-        /// </summary>
-        /// <param name="fileName">로드할 파일 이름</param>
-        /// <typeparam name="T">로드할 데이터 타입</typeparam>
-        /// <returns name="T">T타입 오브젝트 리턴</returns>
-        public static T LoadJsonData<T>(string fileName)
+        public static T LoadJsonData<T>(string loadPath, string fileName)
         {
-            return LoadJsonDataFromFile<T>(fileName);
+            return LoadJsonDataFromFile<T>(loadPath, fileName);
         }
 
-        /// <summary>
-        /// 주어진 Topic(디렉토리)에서 모든 파일 이름을 가져오는 함수
-        /// </summary>
-        /// <param name="topic">Topic(디렉토리)</param>
-        /// <returns>파일 이름이 담긴 리스트</returns>
-        private static List<string> GetPixelArtsNames(string topic)
+        private static List<string> GetPixelArtNames(string topic)
         {
-            List<string> fileNames = new List<string>();
+            var fileNames = new List<string>();
 
-            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(BasePath, topic));
-            
+            var directoryInfo = new DirectoryInfo(Path.Combine(GalleryDataPath, topic));
+
             foreach (var file in directoryInfo.GetFiles())
             {
                 fileNames.Add(Path.GetFileNameWithoutExtension(file.Name));
@@ -64,16 +40,12 @@ namespace LTH.ColorMatch.Managers
 
             return fileNames;
         }
-        /// <summary>
-        /// BasePath의 모든 Topic 이름을 가져오는 함수
-        /// </summary>
-        /// <returns>Topic 이름이 담긴 리스트</returns>
         private static List<string> GetTopicNames()
         {
-            List<string> directoryNames = new List<string>();
+            var directoryNames = new List<string>();
 
-            DirectoryInfo directoryInfo = new DirectoryInfo(BasePath);
-            
+            var directoryInfo = new DirectoryInfo(GalleryDataPath);
+
             foreach (var dir in directoryInfo.GetDirectories())
             {
                 directoryNames.Add(dir.Name);
@@ -81,41 +53,34 @@ namespace LTH.ColorMatch.Managers
 
             return directoryNames;
         }
-
-        /// <summary>
-        /// JSON 데이터를 파일로 저장하는 함수
-        /// </summary>
-        /// <param name="topic">저장할 Topic</param>
-        /// <param name="pixelArt">저장할 PixelArt</param>
-        /// <param name="jsonData">저장할 JSON 데이터</param>
-        private static void SaveJsonDataToFile(string topic,string pixelArt, string jsonData)
+        private static void SaveJsonDataToFile(string savePath, string fileName, string jsonData)
         {
-            string path = Path.Combine(BasePath, topic, $"{pixelArt}.json");
+            var path = Path.Combine(savePath, $"{fileName}.json");
+
+            if (!Directory.Exists(savePath))
+            {
+                Directory.CreateDirectory(savePath);
+            }
+            
             Debug.Log(path);
-            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(jsonData);
-            string code = System.Convert.ToBase64String(bytes);
+            var bytes = System.Text.Encoding.UTF8.GetBytes(jsonData);
+            var code = System.Convert.ToBase64String(bytes);
 
             File.WriteAllText(path, code);
         }
-        /// <summary>
-        /// JSON 데이터를 로드하는 함수
-        /// </summary>
-        /// <typeparam name="T">로드할 데이터 타입</typeparam>
-        /// <param name="fileName">로드할 파일 이름</param>
-        /// <returns>T 타입 오브젝트 리턴</returns>
-        private static T LoadJsonDataFromFile<T>(string fileName)
+        private static T LoadJsonDataFromFile<T>(string loadPath, string fileName)
         {
-            string path = Path.Combine(BasePath, $"{fileName}.json");
+            var path = Path.Combine(loadPath, $"{fileName}.json");
 
-            T loadData = default(T);
+            T loadData = default;
 
-            if (FileExists(fileName))
+            if (FileExists(loadPath,fileName))
             {
-                using (StreamReader reader = new StreamReader(path))
+                using (var reader = new StreamReader(path))
                 {
-                    string code = reader.ReadToEnd();
-                    byte[] bytes = System.Convert.FromBase64String(code);
-                    string loadJson = System.Text.Encoding.UTF8.GetString(bytes);
+                    var code = reader.ReadToEnd();
+                    var bytes = System.Convert.FromBase64String(code);
+                    var loadJson = System.Text.Encoding.UTF8.GetString(bytes);
                     loadData = JsonConvert.DeserializeObject<T>(loadJson);
                 }
             }
@@ -126,14 +91,9 @@ namespace LTH.ColorMatch.Managers
 
             return loadData;
         }
-        /// <summary>
-        /// 파일이 존재하는지 확인합니다.
-        /// </summary>
-        /// <param name="fileName">파일 이름</param>
-        /// <returns>파일의 존재 여부</returns>
-        private static bool FileExists(string fileName)
+        private static bool FileExists(string checkPath,string fileName)
         {
-            string path = Path.Combine(BasePath,$"{fileName}.json");
+            var path = Path.Combine(checkPath, $"{fileName}.json");
             return File.Exists(path);
         }
     }
