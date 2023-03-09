9dcf894cd50714e85465b6e781d80e60f9765fe9
LimTaeHun <lsw463@naver.com>
2023-03-09 10:45:01 +0900

    Add: PixelArtUtill 스크립트 추가

---
+++ Assets/01.Scripts/Utills/PixelArtUtill.cs
@@ -0,0 +1,17 @@
+namespace LTH.ColorMatch.Utill
+{
+    public class PixelArtUtill
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
