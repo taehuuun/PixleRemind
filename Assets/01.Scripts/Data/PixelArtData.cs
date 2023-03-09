2ab6345bc40409f59a0d34d9a17f13c77e2463a3
LimTaeHun <lsw463@naver.com>
2023-03-09 11:40:04 +0900

    Modify: 기존 PixelArtData의 remainPixel변수를 ColorData로 이동

--- Assets/01.Scripts/Data/PixelArtData.cs
+++ Assets/01.Scripts/Data/PixelArtData.cs
@@ -1,6 +1,5 @@
 using System;
 using LTH.ColorMatch.Enums;
-using UnityEngine.Serialization;
 
 namespace LTH.ColorMatch.Data
 {
@@ -11,19 +10,17 @@ namespace LTH.ColorMatch.Data
         public string title;
         public int size;
         public int fillCount;
-        public int remainPixel;
         public bool complete;
         public Difficulty difficulty;
         public ColorData colorData;
 
 
-        public PixelArtData(GalleryTopic gt, string t, ColorData c, int s,int cnt,int remain,bool cp ,Difficulty d)
+        public PixelArtData(GalleryTopic gt, string t, ColorData c, int s,int cnt, bool cp ,Difficulty d)
         {
             topic = gt;
             title = t;
             colorData = c;
             size = s;
-            remainPixel = remain;
             fillCount = cnt;
             complete = cp;
             difficulty = d;
