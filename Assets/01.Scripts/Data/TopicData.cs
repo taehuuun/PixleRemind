bb76c2ff0cf314616b4e4376db55eedce98d48c6
LimTaeHun <lsw463@naver.com>
2023-03-10 10:57:35 +0900

    Add: TopicData.cs 추가

--- Assets/01.Scripts/Data/TopicData.cs
+++ Assets/01.Scripts/Data/TopicData.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class TopicData : MonoBehaviour
+namespace LTH.ColorMatch.Data
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TopicData : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
