646dec014b0078a7ef13282e0f8e2242f153fd52
LimTaeHun <lsw463@naver.com>
2023-03-10 15:24:13 +0900

    Feat: FirebaseManager.cs db인스턴트 초기화 코드 구현

--- Assets/01.Scripts/Manager/FirebaseManager.cs
+++ Assets/01.Scripts/Manager/FirebaseManager.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
-
+using Firebase.Firestore;
 namespace LTH.ColorMatch.Managers
 {
     public class FirebaseManager : MonoBehaviour
     {
+        FirebaseFirestore db;
+
+        private void Start()
+        {
+            db = FirebaseFirestore.DefaultInstance;
+        }
     }
 }
