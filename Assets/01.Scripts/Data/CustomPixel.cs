ffebc384e681df622829d819cff8432a2ab946a9
LimTaeHun <lsw463@naver.com>
2023-03-24 22:16:57 +0900

    Feat: PixelCoord클래스 추가 및 CustomPixel클래스 수정

--- Assets/01.Scripts/Data/CustomPixel.cs
+++ Assets/01.Scripts/Data/CustomPixel.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using Firebase.Firestore;
+using UnityEditor.Tilemaps;
 
 namespace LTH.ColorMatch.Data
 {
@@ -19,42 +21,48 @@ namespace LTH.ColorMatch.Data
             A = a;
         }
     }
+
+    [FirestoreData, Serializable]
+    public class PixelCoord
+    {
+        [FirestoreProperty] public int X { get; set; }
+        [FirestoreProperty] public int Y { get; set; }
+        
+        public PixelCoord() {}
+        public PixelCoord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
     
     [FirestoreData, Serializable]
     public class CustomPixel
     {
         [FirestoreProperty] public ColorValue OriginalColor { get; set; }
         [FirestoreProperty] public ColorValue GrayColor { get; set; }
-        [FirestoreProperty] public bool IsFilled { get; set; }
-        [FirestoreProperty] public int X { get; set;  }
-        [FirestoreProperty] public int Y { get; set; }
+        [FirestoreProperty] public List<PixelCoord> PixelCoords { get; set; } = new List<PixelCoord>();
 
         public CustomPixel()
         {
             OriginalColor = new ColorValue(0, 0, 0, 0);
             GrayColor = new ColorValue(0, 0, 0, 0);
-            IsFilled = false;
         }
         public CustomPixel(ColorValue colorValue)
         {
-            float grayValue = GetGrayValue(colorValue.R, colorValue.G, colorValue.B, colorValue.A);
+            float grayValue = GetGrayValue(colorValue.R, colorValue.G, colorValue.B);
             
             OriginalColor = new ColorValue(colorValue.R, colorValue.G, colorValue.B, colorValue.A);
             GrayColor = new ColorValue(grayValue, grayValue, grayValue, colorValue.A);
-            IsFilled = false;
         }
-
-        public CustomPixel(float r, float g, float b, float a, int x, int y)
+        public CustomPixel(float r, float g, float b, float a)
         {
-            float grayValue = GetGrayValue(r,g,b,a);
+            float grayValue = GetGrayValue(r,g,b);
             
             OriginalColor = new ColorValue(r, g, b, a);
             GrayColor = new ColorValue(grayValue, grayValue, grayValue, a);
-            IsFilled = false;
-            X = x;
-            Y = y;
         }
-        private float GetGrayValue(float r,float g,float b,float a)
+        private float GetGrayValue(float r,float g,float b)
         {
             return (r + g + b) / 3;
         }
