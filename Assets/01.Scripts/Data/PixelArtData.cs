a24b905b51d394634a0969bc51b402e1253b5344
LimTaeHun <lsw463@naver.com>
2023-03-31 03:45:35 +0900

    Feat: PlayTime 프로퍼티 추가 및 생성자 수정, PixelArtUtill 코드 수정

--- Assets/01.Scripts/Data/PixelArtData.cs
+++ Assets/01.Scripts/Data/PixelArtData.cs
@@ -11,20 +11,22 @@ namespace LTH.ColorMatch.Data
         [FirestoreProperty] public GalleryTopic Topic{get;set;}
         [FirestoreProperty] public string Title{get;set;}
         [FirestoreProperty] public string ThumbnailData{get;set;}
+        [FirestoreProperty] public string PlayTime { get; set; }
         [FirestoreProperty] public int Size{get;set;}
         [FirestoreProperty] public int RemainingFills{get;set;}
         [FirestoreProperty] public bool IsCompleted{get;set;}
         [FirestoreProperty] public Difficulty Difficulty{get;set;}
         [FirestoreProperty] public PixelColorData PixelColorData{get;set;}
-
+        
         public PixelArtData()
         {
         }
-        public PixelArtData(GalleryTopic topic, string title, string thumbnailData, int size, int remainingFills, bool isComplete, Difficulty difficulty, PixelColorData pixelColorData)
+        public PixelArtData(GalleryTopic topic, string title, string thumbnailData, string playTime,int size, int remainingFills, bool isComplete, Difficulty difficulty, PixelColorData pixelColorData)
         {
             Topic = topic;
             Title = title;
             ThumbnailData = thumbnailData;
+            PlayTime = playTime;
             Size = size;
             RemainingFills = remainingFills;
             IsCompleted = isComplete;
@@ -38,6 +40,7 @@ namespace LTH.ColorMatch.Data
                 { "Topic", Topic },
                 { "Title", Title },
                 { "ThumbnailData", ThumbnailData },
+                {"PlayTime",PlayTime},
                 { "Size", Size },
                 { "RemainingFills", RemainingFills },
                 { "IsCompleted", IsCompleted },
