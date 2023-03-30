d317a834f58bbf2ef41505b735bf4670ca2ab6f5
LimTaeHun <lsw463@naver.com>
2023-03-31 04:38:27 +0900

    Refactor: PlayTime 변수 타입 수정

--- Assets/01.Scripts/Data/PixelArtData.cs
+++ Assets/01.Scripts/Data/PixelArtData.cs
@@ -11,7 +11,7 @@ namespace LTH.ColorMatch.Data
         [FirestoreProperty] public GalleryTopic Topic{get;set;}
         [FirestoreProperty] public string Title{get;set;}
         [FirestoreProperty] public string ThumbnailData{get;set;}
-        [FirestoreProperty] public string PlayTime { get; set; }
+        [FirestoreProperty] public int PlayTime { get; set; }
         [FirestoreProperty] public int Size{get;set;}
         [FirestoreProperty] public int RemainingFills{get;set;}
         [FirestoreProperty] public bool IsCompleted{get;set;}
@@ -21,7 +21,7 @@ namespace LTH.ColorMatch.Data
         public PixelArtData()
         {
         }
-        public PixelArtData(GalleryTopic topic, string title, string thumbnailData, string playTime,int size, int remainingFills, bool isComplete, Difficulty difficulty, PixelColorData pixelColorData)
+        public PixelArtData(GalleryTopic topic, string title, string thumbnailData, int playTime,int size, int remainingFills, bool isComplete, Difficulty difficulty, PixelColorData pixelColorData)
         {
             Topic = topic;
             Title = title;
