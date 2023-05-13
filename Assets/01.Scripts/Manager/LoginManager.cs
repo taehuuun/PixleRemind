ec741ec9adac393c61b58db47b7c4f34f952d79d
LimTaeHun <lsw463@naver.com>
2023-05-14 02:02:04 +0900

    Remove: GooglePlayGamesPlugin v0.11.01 제거

--- Assets/01.Scripts/Manager/LoginManager.cs
+++ Assets/01.Scripts/Manager/LoginManager.cs
@@ -1,12 +1,7 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace LTH.ColorMatch.Managers
 {
-    public class LoginManager : MonoBehaviour
-    {
-        
-    }
 }
