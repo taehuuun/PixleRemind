4d78600152b4d47e81868e8c0afbbc3bc7151580
LimTaeHun <lsw463@naver.com>
2023-05-16 04:18:30 +0900

    Feat: PlayGamesPlatform 초기화를 위한 Init함수 구현

--- Assets/01.Scripts/Utills/GPGSUtill.cs
+++ Assets/01.Scripts/Utills/GPGSUtill.cs
@@ -1,7 +1,27 @@
-using System.Drawing.Text;
-using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 
 public class GPGSUtill
 {
-    
+    /// <summary>
+    /// 구글 플레이 서비스에서 사용되는 PlayGamesPlatform 객체를 초기화 시키는 함수
+    /// </summary>
+    public static void Init()
+    {
+        // PlayGamesClientConfiguration을 사용하여 구성 객체를 만듭니다.
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+            .RequestEmail()                 // 사용자의 이메일 주소 요청
+            .RequestIdToken()               // 사용자 인증에 필요한 토큰 요청
+            .RequestServerAuthCode(false)   // 인증 코드를 요청하지 않도록 설정합니다.
+            .Build();
+        
+        // config 인스턴스를 전달하여 PlayGamesPlatform 객체를 초기화
+        PlayGamesPlatform.InitializeInstance(config);
+        
+        // 디버그 로그를 비활성화
+        PlayGamesPlatform.DebugLogEnabled = false;
+        
+        // PlayGamesPlatform을 활성화
+        PlayGamesPlatform.Activate();
+    }
 }
