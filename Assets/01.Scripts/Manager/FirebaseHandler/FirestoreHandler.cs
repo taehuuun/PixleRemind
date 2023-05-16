3100093b26a431275165d2b8df3505dc19bc39bd
LimTaeHun <lsw463@naver.com>
2023-05-16 21:40:26 +0900

    Refactor: FirebaseManager의 기존 Firestore 기능들을 FirestoreHandler로 이동

--- Assets/01.Scripts/Manager/FirebaseHandler/FirestoreHandler.cs
+++ Assets/01.Scripts/Manager/FirebaseHandler/FirestoreHandler.cs
@@ -1,8 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Firebase.Firestore;
+using LTH.ColorMatch.Data;
 using UnityEngine;
 
 namespace LTH.ColorMatch.Handlers
 {
-    public class FirestoreHandler : MonoBehaviour
+    public class FirestoreHandler
     {
+        private FirebaseFirestore _firestore;
+
+        public FirestoreHandler()
+        {
+            _firestore = FirebaseFirestore.DefaultInstance;
+        }
+        
+        public async Task<bool> CheckDocumentExists(string collection, string document)
+        {
+            try
+            {
+                var docRef = _firestore.Collection(collection).Document(document);
+                var docSnapShot = await docRef.GetSnapshotAsync();
+
+                return docSnapShot.Exists;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+        }
+        public async Task<bool> CheckCollectionExists(string collection)
+        {
+            try
+            {
+                var colRef = _firestore.Collection(collection);
+                var colSnapShot = await colRef.GetSnapshotAsync();
+
+                return colSnapShot.Count > 0;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+        }
+        public async Task<TopicData> GetTopicData(string collection, string document)
+        {
+            try
+            {
+                var docRef = _firestore.Collection(collection).Document(document);
+                var docSnapShot = await docRef.GetSnapshotAsync();
+
+                if (!docSnapShot.Exists)
+                {
+                    Debug.LogError("Document does not Exists");
+                    return null;
+                }
+
+                var topicData = docSnapShot.ConvertTo<TopicData>();
+                return topicData;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+        }
+        public async Task<List<TopicData>> GetAllTopicData()
+        {
+            try
+            {
+                var snapshot = await _firestore.Collection("GalleryData").GetSnapshotAsync();
+                var topicDataList = new List<TopicData>();
+
+                foreach (var document in snapshot.Documents)
+                {
+                    Debug.Log(document.Id);
+                    var topicData = document.ConvertTo<TopicData>();
+                    topicDataList.Add(topicData);
+                }
+
+                return topicDataList;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+        }
+        public async Task AddTopicData(string collectionName, string documentName, TopicData topicData)
+        {
+            try
+            {
+                var documentReference = _firestore.Collection(collectionName).Document(documentName);
+                
+                await documentReference.SetAsync(topicData);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+        }
+        public async Task UpdateTopicData(string collectionName, string documentName, TopicData topicData)
+        {
+            try
+            {
+                var documentReference = _firestore.Collection(collectionName).Document(documentName);
+                
+                await documentReference.SetAsync(topicData, SetOptions.MergeAll);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+                throw;
+            }
+        }
     }
 }
