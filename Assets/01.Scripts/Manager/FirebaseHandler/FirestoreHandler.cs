671a154c03513914cc6ab22a0f32fae8abf9aa3c
LimTaeHun <lsw463@naver.com>
2023-05-16 20:31:21 +0900

    Refactor: FirestoreHandler.cs 네임스페이스 수정 및 불필요 네임스페이스, 함수 제거

--- Assets/01.Scripts/Manager/FirebaseHandler/FirestoreHandler.cs
+++ Assets/01.Scripts/Manager/FirebaseHandler/FirestoreHandler.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FirestoreHandler : MonoBehaviour
+namespace LTH.ColorMatch.Handlers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FirestoreHandler : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
