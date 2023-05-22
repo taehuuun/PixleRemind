84021fbc42836a8b365277c41d7bddec21fcfaef
LimTaeHun <lsw463@naver.com>
2023-05-23 01:12:43 +0900

    Feat: FirebaseManager 기능 구현 및 주석 추가

--- Assets/01.Scripts/Manager/LoginManager.cs
+++ Assets/01.Scripts/Manager/LoginManager.cs
@@ -1,12 +1,11 @@
-using System;
-using UnityEngine;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace LTH.ColorMatch.Managers
 {
     public class LoginManager : MonoBehaviour
     {
-        private void Start()
+        private async void Start()
         {
             Debug.Log("GPGS Init");
             // GPGSUtill을 초기화
@@ -14,61 +13,25 @@ namespace LTH.ColorMatch.Managers
 
             // LoginAsync를 호출
             Debug.Log("LoginAsync");
-            LoginAsync(); 
-        }
-
-        private async void LoginAsync()
-        {
-            try
-            {
-                Debug.Log("LoginAsync Start");
-                // Login 메소드를 호출하고 반환값인 idToken을 받음
-                string idToken = await Login(); 
-                
-                Debug.Log("!!!!!");
-                // Firebase authentication을 시도하고 반환값인 firebaseLoginSuccess를 받음
-                bool firebaseLoginSuccess = await FirebaseManager.ins.FireAuth.TryFirebaseLogin(idToken);
-                
-                Debug.Log($"@@@@@@ : {idToken}");
-                
-                if (!firebaseLoginSuccess)
-                {
-                    // Firebase authentication이 실패하면 로그를 출력
-                    Debug.LogError("Firebase Login Failed");
-                }
-            }
-            catch (Exception e)
-            {
-                // Login 메소드 실행 중 예외가 발생하면 로그를 출력
-                Debug.LogError($"Google Game Services login failed: {e.Message}"); 
-            }
-        }
-
-        private Task<string> Login()
-        {
-            Debug.Log("Login Start");
-            // TaskCompletionSource를 생성
-            var tcs = new TaskCompletionSource<string>(); 
             
-            // GPGSUtill 로그인 메소드에 콜백을 등록
-            GPGSUtill.Login((success, token) => 
+            await Login(); 
+        }
+        
+        /// <summary>
+        /// Login 메서드
+        /// </summary>
+        private async Task Login()
+        {
+            // GPGSUtill을 통해 로그인을 시도
+            if (await GPGSUtill.LoginAsync())
             {
-                Debug.Log($"Login : {success}");
-                if (success)
-                {
-                    // 성공하면 반환값을 TaskCompletionSource에 설정
-                    tcs.SetResult(token); 
-                }
-                else
-                {
-                    // 실패하면 예외를 TaskCompletionSource에 설정
-                    tcs.SetException(new Exception("Google Game Services login failed.")); 
-                }
-            });
-            
-            Debug.Log($"Login Result: {tcs.Task}");
-            // TaskCompletionSource.Task를 반환
-            return tcs.Task; 
+                // 로그인이 성공하면, Firebase Auth 로그인을 시도
+                FirebaseManager.ins.FireAuth.TryFirebaseLogin();
+                
+                // GPGSUtill의 IdToken과 FUID를 로그로 출력
+                Debug.Log($"IdToken : {GPGSUtill.IdToken}");
+                Debug.Log($"FUID : {FirebaseManager.ins.FireAuth.FUID}");
+            }
         }
     }
 }
