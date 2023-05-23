166092893f65e77ca61967fcc6a98093ab4102ee
LimTaeHun <lsw463@naver.com>
2023-05-24 08:28:14 +0900

    Refactor: TryFirebaseLogin 메서드를 비동기 메서드로 수정

--- Assets/01.Scripts/Manager/Firebase/TopicDataController.cs
+++ Assets/01.Scripts/Manager/Firebase/TopicDataController.cs
@@ -1,9 +1,21 @@
+using System.Threading.Tasks;
 using UnityEngine;
+using LTH.ColorMatch.Managers.FirebaseHandlers;
 
 namespace LTH.ColorMatch.Managers.FirebaseController
 {
     public class TopicDataController : MonoBehaviour
     {
-        
+        private readonly FirestoreHandler _firestore;
+
+        public TopicDataController(FirestoreHandler firestore)
+        {
+            _firestore = firestore;
+        }
+
+        public async Task CheckAndUpdateTopic()
+        {
+            
+        }
     }
 }
