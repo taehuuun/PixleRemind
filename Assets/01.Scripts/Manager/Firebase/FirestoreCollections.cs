7b9e9c73a965fd50d430af7dccdce29ef9f8997d
LimTaeHun <lsw463@naver.com>
2023-05-25 06:14:06 +0900

    Feat: 현재 Firestore에서 사용중인 컬렉션들을 상수화

--- Assets/01.Scripts/Manager/Firebase/FirestoreCollections.cs
+++ Assets/01.Scripts/Manager/Firebase/FirestoreCollections.cs
@@ -2,5 +2,7 @@ namespace LTH.PixelRemind.Managers.Firebase.Collections
 {
    public static class FirestoreCollections
    {
+      public const string GalleryData = "GalleryData";
+      public const string UserData = "UserData";
    }
 }
