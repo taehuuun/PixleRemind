04e8f8349bfd305b49621af144f7ba8642c983af
LimTaeHun <lsw463@naver.com>
2023-05-25 11:31:36 +0900

    Refactor: 기존 Topic 프로퍼티를 ID로 수정

--- Assets/01.Scripts/Data/PixelArtData.cs
+++ Assets/01.Scripts/Data/PixelArtData.cs
@@ -8,8 +8,7 @@ namespace LTH.PixelRemind.Data
     [FirestoreData, Serializable]
     public class PixelArtData
     {
-        [FirestoreProperty] public GalleryTopic Topic{get;set;}
-        [FirestoreProperty] public string Title{get;set;}
+        [FirestoreProperty] public string ID {get;set;}
         [FirestoreProperty] public string ThumbnailData{get;set;}
         [FirestoreProperty] public int PlayTime { get; set; }
         [FirestoreProperty] public int Size{get;set;}
@@ -17,35 +16,16 @@ namespace LTH.PixelRemind.Data
         [FirestoreProperty] public Difficulty Difficulty{get;set;}
         [FirestoreProperty] public PixelColorData PixelColorData{get;set;}
         
-        public PixelArtData()
+        public PixelArtData() { }
+        public PixelArtData(string id, string thumbnailData, int playTime,int size, bool isComplete, Difficulty difficulty, PixelColorData pixelColorData)
         {
-        }
-        public PixelArtData(GalleryTopic topic, string title, string thumbnailData, int playTime,int size, bool isComplete, Difficulty difficulty, PixelColorData pixelColorData)
-        {
-            Topic = topic;
-            Title = title;
+            ID = id;
             ThumbnailData = thumbnailData;
             PlayTime = playTime;
             Size = size;
             IsCompleted = isComplete;
             Difficulty = difficulty;
             PixelColorData = pixelColorData;
-        }
-        public Dictionary<string, object> ToDictionary()
-        {
-            var dictionary = new Dictionary<string, object>
-            {
-                { "Topic", Topic },
-                { "Title", Title },
-                { "ThumbnailData", ThumbnailData },
-                {"PlayTime",PlayTime},
-                { "Size", Size },
-                { "IsCompleted", IsCompleted },
-                { "Difficulty", Difficulty },
-                { "PixelColorData", PixelColorData.ToDictionary() }
-            };
-
-            return dictionary;
         }
     }
 }
