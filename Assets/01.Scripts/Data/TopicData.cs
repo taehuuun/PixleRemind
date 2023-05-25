8c294ca76846d8101176c1ed7c55290ac4d7058c
LimTaeHun <lsw463@naver.com>
2023-05-25 11:20:14 +0900

    Refactor: 일부 프로퍼티 수정

--- Assets/01.Scripts/Data/TopicData.cs
+++ Assets/01.Scripts/Data/TopicData.cs
@@ -1,46 +1,35 @@
 using System;
 using System.Collections.Generic;
 using Firebase.Firestore;
-using LTH.PixelRemind.Enums;
 
 namespace LTH.PixelRemind.Data
 {
     [FirestoreData, Serializable]
     public class TopicData
     {
-        [FirestoreProperty] public GalleryTopic Topic {get;set;}
+        [FirestoreProperty] public string ID { get; set; }
         [FirestoreProperty] public string ThumbData {get;set;}
         [FirestoreProperty] public int CompleteCount {get;set;}
         [FirestoreProperty] public int TotalCount {get;set;}
         [FirestoreProperty] public int ThumbSize { get; set; }
         [FirestoreProperty] public bool Complete {get;set;}
+        [FirestoreProperty] public bool Updateable { get; set; }
+        [FirestoreProperty] public DateTime LastUpdated { get; set; }
         [FirestoreProperty] public List<PixelArtData> PixelArtDatas { get; set; }
 
         public TopicData() {}
-        public TopicData(GalleryTopic topic, string thumbData, int completeCount, int totalCount,int thumbSize, bool complete
-            , List<PixelArtData> pixelArtDatas)
+        public TopicData(string id,string thumbData, int completeCount, int totalCount,int thumbSize, bool complete, bool updateable
+            , DateTime lastUpdated, List<PixelArtData> pixelArtDatas)
         {
-            Topic = topic;
+            ID = id;
             ThumbData = thumbData;
             CompleteCount = completeCount;
             TotalCount = totalCount;
             ThumbSize = thumbSize;
             Complete = complete;
+            Updateable = updateable;
+            LastUpdated = lastUpdated;
             PixelArtDatas = pixelArtDatas;
-        }
-                
-        public Dictionary<string, object> ToDictionary()
-        {
-            var dictionary = new Dictionary<string, object>
-            {
-                { "Topic", Topic },
-                { "ThumbData", ThumbData },
-                { "CompleteCount", CompleteCount },
-                { "TotalCount", TotalCount },
-                { "Complete", Complete },
-                { "PixelArtDatas", PixelArtDatas }
-            };
-            return dictionary;
         }
     }    
 }
