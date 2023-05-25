b42b0a27919469d01f80505c3b01605bb8415a51
LimTaeHun <lsw463@naver.com>
2023-05-25 17:33:06 +0900

    Feat: UnlockCondition 필요 프로퍼티 추가

--- Assets/01.Scripts/Data/Conditions/UnlockCondition.cs
+++ Assets/01.Scripts/Data/Conditions/UnlockCondition.cs
@@ -1,8 +1,22 @@
+using System;
+using Firebase.Firestore;
+using LTH.PixelRemind.Enums;
 using UnityEngine;
 
 namespace LTH.PixelRemind.Data.Conditions
 {
+    [FirestoreData, Serializable]
     public class UnlockCondition : MonoBehaviour
     {
+        [FirestoreProperty] public UnlockType Type { get; set; }
+        [FirestoreProperty] public int Count { get; set; }
+        
+        public UnlockCondition (){}
+
+        public UnlockCondition(UnlockType type, int count)
+        {
+            Type = type;
+            Count = count;
+        }
     }
 }
