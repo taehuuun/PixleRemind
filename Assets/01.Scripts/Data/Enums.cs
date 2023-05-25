da6d28efd802eea40137c6d9f7248301750eddb7
LimTaeHun <lsw463@naver.com>
2023-05-25 16:40:53 +0900

    Feat: UnlockType 추가

--- Assets/01.Scripts/Data/Enums.cs
+++ Assets/01.Scripts/Data/Enums.cs
@@ -4,4 +4,5 @@ namespace LTH.PixelRemind.Enums
     public enum Difficulty {Easy, Normal, Hard}
     public enum GalleryPage { Topic, PixelArt, ColorMatch }
     public enum FadeType {In, Out}
+    public enum UnlockType {CoinPurchase, GemPurchase, AdWatch}
 }
