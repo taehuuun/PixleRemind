edfbaea46ef3fbd69d762ab6e41bdf6594ab7547
LimTaeHun <lsw463@naver.com>
2023-05-28 10:40:02 +0900

    Feat: Login 메서드 수정, Initialize 메서드 구현

--- Assets/01.Scripts/Manager/LoginManager.cs
+++ Assets/01.Scripts/Manager/LoginManager.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Threading.Tasks;
+using LTH.PixelRemind.Data;
+using LTH.PixelRemind.Managers.Data;
 using LTH.PixelRemind.Managers.Firebase;
+using LTH.PixelRemind.Managers.Firebase.Collections;
 using UnityEngine;
 
 namespace LTH.PixelRemind.Managers.Login
@@ -24,12 +28,14 @@ namespace LTH.PixelRemind.Managers.Login
         /// </summary>
         private async Task Login()
         {
+#if UNITY_ANDROID && !UNITY_EDITOR
             // GPGSUtill을 통해 로그인을 시도
             if (await GPGSUtill.LoginAsync())
             {
                 // 로그인이 성공하면, Firebase Auth 로그인을 시도
                 if (await FirebaseManager.ins.FireAuth.TryFirebaseLogin())
                 {
+                    await Initialize();
                     // GPGSUtill의 IdToken과 FUID를 로그로 출력
                     Debug.Log($"IdToken : {GPGSUtill.IdToken}");
                     Debug.Log($"FUID : {FirebaseManager.ins.FireAuth.FUID}");
@@ -42,6 +48,34 @@ namespace LTH.PixelRemind.Managers.Login
             else
             {
                 Debug.LogError("GPGS 로그인 실패");
+            }
+#else
+            await Initialize();
+#endif
+        }
+
+        private async Task Initialize()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            string FUID = FirebaseManager.ins.FireAuth.FUID;
+#else
+            string FUID = "Test";
+#endif
+            bool userDataExists =
+                await FirebaseManager.ins.Firestore.CheckDocumentExists(FirestoreCollections.UserData, FUID);
+
+            if (!userDataExists)
+            {
+                Debug.Log("최초 접속 유저");
+                DataManager.Instance.userData = new UserData();
+                DataManager.Instance.userData.LastUpdated = DateTime.Now;
+                await FirebaseManager.ins.Firestore.AddData(FirestoreCollections.UserData,FUID, DataManager.Instance.userData);
+            }
+            else
+            {
+                Debug.Log("기존 유저");
+                DataManager.Instance.userData =
+                    await FirebaseManager.ins.Firestore.GetData<UserData>(FirestoreCollections.UserData, FUID);
             }
         }
     }
