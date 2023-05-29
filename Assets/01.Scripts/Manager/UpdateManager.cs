b1df80a321ddbb12c35b913afece72ca93808f92
LimTaeHun <lsw463@naver.com>
2023-05-30 05:52:21 +0900

    Feat: 기본적인 CheckForUpdated 메서드와 DownloadTopicData 메서드 구현

--- Assets/01.Scripts/Manager/UpdateManager.cs
+++ Assets/01.Scripts/Manager/UpdateManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using LTH.PixelRemind.Managers.Data;
 using LTH.PixelRemind.Managers.Data.Paths;
 using LTH.PixelRemind.Managers.Firebase;
 using LTH.PixelRemind.Managers.Firebase.Collections;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace LTH.PixelRemind.Managers
@@ -25,24 +27,56 @@ namespace LTH.PixelRemind.Managers
         private async Task CheckForUpdated()
         {
             List<string> localTopicDataIDs = DataManager.Instance.userData.LocalTopicDataIDs;
-
-            List<string> missingDataIDS = _topicDataList.Select(data => data.ID).Except(localTopicDataIDs).ToList();
-
-            List<string> outdatedDataIDs = new List<string>();
-
-            foreach (var id in localTopicDataIDs)
-            {
-                TopicData localData = DataManager.LoadJsonData<TopicData>(DataPath.GalleryDataPath, id);
-                TopicData serverData = _topicDataList.Find(data => data.ID == id);
-
-                if (serverData.LastUpdated > localData.LastUpdated)
-                {
-                    outdatedDataIDs.Add(id);
-                }
-            }
             
-            Debug.Log($"Missing Data : {string.Join(", ",missingDataIDS)}");
-            Debug.Log($"Outdated Data : {string.Join(", ",outdatedDataIDs)}");
+            if (localTopicDataIDs == null || localTopicDataIDs.Count == 0)
+            {
+                List<string> tmpList = new List<string>();
+                foreach (var topicData in _topicDataList)
+                {
+                    tmpList.Add(topicData.Title);
+                }
+                Debug.Log($"Missing Data : {string.Join(", ", tmpList)}");
+            }
+            else
+            {
+                List<string> missingDataIDs = new List<string>();
+                
+                missingDataIDs = _topicDataList.Select(data => data.ID).Except(localTopicDataIDs).ToList();
+                
+                List<string> outdatedDataIDs = new List<string>();
+
+                foreach (var id in localTopicDataIDs)
+                {
+                    TopicData localData = DataManager.LoadJsonData<TopicData>(DataPath.GalleryDataPath, id);
+                    TopicData serverData = _topicDataList.Find(data => data.ID == id);
+
+                    if (serverData.LastUpdated > localData.LastUpdated)
+                    {
+                        outdatedDataIDs.Add(id);
+                    }
+                }
+                
+                Debug.Log($"Missing Data : {string.Join(", ",missingDataIDs)}");
+                Debug.Log($"Outdated Data : {string.Join(", ",outdatedDataIDs)}");
+            }
+        }
+
+        private async Task DownloadTopicData(string topicID)
+        {
+            TopicData serverData = _topicDataList.Find(data => data.ID == topicID);
+            
+            string jsonData = JsonConvert.SerializeObject(serverData);
+            DataManager.SaveJsonData(DataPath.GalleryDataPath,topicID,jsonData);
+            
+            DataManager.Instance.userData.LocalTopicDataIDs.Add(topicID);
+            DataManager.Instance.userData.LastUpdated = DateTime.Now;
+            
+#if UNITY_ANDROID && !UNITY_EDITOR
+             string FUID = FirebaseManager.ins.FireAuth.FUID;
+#else
+            string FUID = "Test";
+#endif
+            await FirebaseManager.ins.Firestore.UpdateData<UserData>(FirestoreCollections.UserData, FUID, DataManager.Instance.userData);
         }
     }
 }
