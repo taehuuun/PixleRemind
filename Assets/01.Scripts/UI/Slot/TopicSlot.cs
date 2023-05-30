a3f9ef2298d841cb7eaefe2598ccfdc7db94edbd
LimTaeHun <lsw463@naver.com>
2023-05-31 01:09:16 +0900

    Restore: TopicSlot 복구

---
+++ Assets/01.Scripts/UI/Slot/TopicSlot.cs
@@ -0,0 +1,30 @@
+using LTH.PixelRemind.Data;
+using LTH.PixelRemind.Managers.Gallery;
+using LTH.PixelRemind.Utill;
+using TMPro;
+using UnityEngine;
+
+namespace LTH.PixelRemind.UI.Slots
+{
+    public class TopicSlot : GallerySlot
+    {
+        public TMP_Text completeCountText;
+        public GameObject completeMark;
+        public TopicData data;
+        public override void OnSlotClick()
+        {
+            foreach (var pixelArtData in data.PixelArtDatas)
+            {
+                GalleryManager.ins.PixelArtDatas.Add(pixelArtData);
+            }
+        }
+
+        public override void SetSlot()
+        {
+            titleText.text = data.ID;
+            completeMark.SetActive(data.Complete);
+            completeCountText.text = $"{data.CompleteCount} / {data.TotalCount}";
+            thumbnailImb.sprite = PixelArtUtill.MakeThumbnail(data.ThumbData, data.ThumbSize);
+        }
+    }
+}
