2b3586df9f2ab3a1a6cae7e2f5e0aef3013f0698
LimTaeHun <lsw463@naver.com>
2023-05-30 20:44:03 +0900

    Feat: UpdateSlot의 기본적인 필드 변수와 SetSlot메서드 구현

--- Assets/01.Scripts/UI/Slot/UpdateSlot.cs
+++ Assets/01.Scripts/UI/Slot/UpdateSlot.cs
@@ -1,9 +1,28 @@
+using LTH.PixelRemind.Data;
+using LTH.PixelRemind.Utill;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LTH.PixelRemind.UI.Slots
 {
     public class UpdateSlot : MonoBehaviour
     {
-
+        public Image thumbnailImg;
+        public Text titleText;
+        public Text introductionText;
+        public GameObject checkIcon;
+        public GameObject updateIcon;
+        public GameObject missingIcon;
+        
+        private TopicData _topicData;
+        
+        public void SetSlot(TopicData topicData, bool update, bool missing)
+        {
+            thumbnailImg.sprite = PixelArtUtill.MakeThumbnail(topicData.ThumbData, topicData.ThumbSize);
+            titleText.text = $"{topicData.Title} 픽셀아트 {topicData.TotalCount}개";
+            introductionText.text = topicData.Introduction;
+            updateIcon.SetActive(update);
+            missingIcon.SetActive(missing);
+        }
     }
 }
