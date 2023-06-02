aea7a0779e8582dc3b827cb98da57bccda0c4a58
LimTaeHun <lsw463@naver.com>
2023-06-02 19:49:03 +0900

    Feat: 추가된 Task들을 실행 시키는 코드 추가

--- Assets/01.Scripts/UI/LoadingUI.cs
+++ Assets/01.Scripts/UI/LoadingUI.cs
@@ -20,6 +20,9 @@ namespace LTH.PixelRemind.UI
             LoadingTaskManager.Instance.AddTask(GPGSUtil.Init,"구글 플레이 서비스 초기화...");
             LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
             LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
+
+            StartCoroutine(ShowLoadingProgress());
+            _ = LoadingTaskManager.Instance.RunTasks();
         }
 
         private IEnumerator ShowLoadingProgress()
