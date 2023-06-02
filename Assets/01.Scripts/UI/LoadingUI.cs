2203882d1e489e1bb3f1a2469f5c50b139b98c51
LimTaeHun <lsw463@naver.com>
2023-06-02 19:44:50 +0900

    Feat: _loadingBar, _taskText / Start 메서드에서 초기화

--- Assets/01.Scripts/UI/LoadingUI.cs
+++ Assets/01.Scripts/UI/LoadingUI.cs
@@ -15,6 +15,8 @@ namespace LTH.PixelRemind.UI
         
         private void Start()
         {
+            _loadingBar.fillAmount = 0f;
+            _taskText.text = "";
             LoadingTaskManager.Instance.AddTask(GPGSUtil.Init,"구글 플레이 서비스 초기화...");
             LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
             LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
