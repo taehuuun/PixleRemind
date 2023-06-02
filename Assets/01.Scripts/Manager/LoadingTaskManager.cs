5e4a7c90d83e8121bf436d845c785dc0cf191ca2
LimTaeHun <lsw463@naver.com>
2023-06-02 12:27:59 +0900

    Feat: LoadingTaskManager.cs 기능 구현

--- Assets/01.Scripts/Manager/LoadingTaskManager.cs
+++ Assets/01.Scripts/Manager/LoadingTaskManager.cs
@@ -1,9 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LTH.PixelRemind.Data;
 using UnityEngine;
 
 namespace LTH.PixelRemind.Managers
 {
     public class LoadingTaskManager : MonoBehaviour
     {
+        private static LoadingTaskManager _instance;
+        public static LoadingTaskManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = nameof(LoadingTaskManager);
+                    _instance = obj.AddComponent<LoadingTaskManager>();
+                }
+
+                return _instance;
+            }
+        }
+        
+        public float TaskProgress { get; private set; }
+        public bool AllTaskComplete { get; private set; }
+        public string CurrentTask { get; private set; }
+
+        private readonly List<TaskData> _tasks = new List<TaskData>();
+        
+        public void AddTask(Func<Task> task, string name)
+        {
+            _tasks.Add(new TaskData(task,name));
+        }
+
+        public async Task RunTasks()
+        {
+            for (int i = 0; i < _tasks.Count; i++)
+            {
+                CurrentTask = _tasks[i].Name;
+                await _tasks[i].Task.Invoke();
+                TaskProgress = (float)(i + 1) / _tasks.Count;
+            }
+
+            AllTaskComplete = true;
+        }
+
+        public void ResetTasks()
+        {
+            _tasks.Clear();
+            TaskProgress = 0f;
+            AllTaskComplete = false;
+        }
     }    
 }
 
