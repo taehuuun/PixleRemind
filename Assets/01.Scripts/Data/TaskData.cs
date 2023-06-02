85b0939695ea02539264e83b99cfab64d1b8021e
LimTaeHun <lsw463@naver.com>
2023-06-02 11:25:44 +0900

    Feat: TaskData 프로퍼티 추가 및 생성자 구현

--- Assets/01.Scripts/Data/TaskData.cs
+++ Assets/01.Scripts/Data/TaskData.cs
@@ -1,10 +1,19 @@
+using System;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace LTH.PixelRemind.Data
 {
     public class TaskData : MonoBehaviour
     {
+        public Func<Task> Task { get; private set; }
+        public string Name { get; private set; }
 
+        public TaskData(Func<Task> task, string name)
+        {
+            Task = task;
+            Name = name;
+        }
     }
 }
 
