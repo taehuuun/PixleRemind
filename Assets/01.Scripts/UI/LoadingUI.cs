f0cfcbf4f60e553a7446638a73ba8c232bd63076
LimTaeHun <lsw463@naver.com>
2023-06-16 11:34:32 +0900

    Refactor: 불필요한 주석 코드 제거

--- Assets/01.Scripts/UI/LoadingUI.cs
+++ Assets/01.Scripts/UI/LoadingUI.cs
@@ -15,10 +15,7 @@ namespace LTH.PixelRemind.UI
         {
             _loadingBar.fillAmount = 0f;
             _taskText.text = "";
-            // LoadingTaskManager.Instance.AddTask(GPGSUtil.Init,"구글 플레이 서비스 초기화...");
-            // LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
-            // LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
-
+            
             StartCoroutine(ShowLoadingProgress());
             _ = LoadingTaskManager.Instance.RunTasks();
         }
