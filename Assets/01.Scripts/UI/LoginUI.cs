c3edd4bb6997699acb8dd389d497cf917bc16ac9
LimTaeHun <lsw463@naver.com>
2023-06-16 11:33:42 +0900

    Feat: Start 메서드 구현 / Loading Task 추가 및 LoadingScene 이동

--- Assets/01.Scripts/UI/LoginUI.cs
+++ Assets/01.Scripts/UI/LoginUI.cs
@@ -1,8 +1,17 @@
-using UnityEngine;
+using LTH.PixelRemind.Managers;
+using LTH.PixelRemind.Managers.Login;
+using LTH.PixelRemind.Util;
 
 namespace LTH.PixelRemind.UI
 {
     public class LoginUI : BodyUI
     {
+        private void Start()
+        {
+            LoadingTaskManager.Instance.AddTask(GPGSUtil.Init,"구글 플레이 서비스 초기화...");
+            LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
+            LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
+            MoveScene("LoadingScene");
+        }
     }
 }
