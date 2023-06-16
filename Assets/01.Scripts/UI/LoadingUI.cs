cf51e9094558d01d3d5ac5c7a163dae59e7f267d
LimTaeHun <lsw463@naver.com>
2023-06-16 11:18:26 +0900

    Refactor: ShowLoadingProgress 코루틴 메서드 수정

--- Assets/01.Scripts/UI/LoadingUI.cs
+++ Assets/01.Scripts/UI/LoadingUI.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using LTH.PixelRemind.Managers;
-using LTH.PixelRemind.Managers.Login;
-using LTH.PixelRemind.Util;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,9 +15,9 @@ namespace LTH.PixelRemind.UI
         {
             _loadingBar.fillAmount = 0f;
             _taskText.text = "";
-            LoadingTaskManager.Instance.AddTask(GPGSUtil.Init,"구글 플레이 서비스 초기화...");
-            LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
-            LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
+            // LoadingTaskManager.Instance.AddTask(GPGSUtil.Init,"구글 플레이 서비스 초기화...");
+            // LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
+            // LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
 
             StartCoroutine(ShowLoadingProgress());
             _ = LoadingTaskManager.Instance.RunTasks();
@@ -36,8 +34,8 @@ namespace LTH.PixelRemind.UI
             }
 
             _loadingBar.fillAmount = 1f;
-            _taskText.text = "메인 씬으로 이동중..";
-            MoveScene("MainScene");
+            _taskText.text = $"{LoadingTaskManager.Instance.NextSceneName} 씬으로 이동중..";
+            MoveScene(LoadingTaskManager.Instance.NextSceneName);
         }
     }
 }
