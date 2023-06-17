669da90ced7e189b6ca9f9c5619e67c0c546a85c
LimTaeHun <lsw463@naver.com>
2023-06-17 10:19:45 +0900

    Add: 각 씬 네임 상수형 문자열로 필드 추가

--- Assets/01.Scripts/Config/SceneNames.cs
+++ Assets/01.Scripts/Config/SceneNames.cs
@@ -1,3 +1,6 @@
-public class SceneNames
+public static class SceneNames
 {
+    public const string LoginScene = "00.LoginScene";
+    public const string LoadingScene = "01.LoadingScene";
+    public const string MainScene = "02.MainScene";
 }
