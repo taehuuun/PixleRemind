e7c7a353a6a6303011f7730b41090a07dc8f5ee1
LimTaeHun <lsw463@naver.com>
2023-06-17 10:21:01 +0900

    Refactor: 기존 문자열 이용 코드 SceneNames 정적 클래스를 사용하는 코드로 수정

--- Assets/01.Scripts/UI/Scenes/LoginUI.cs
+++ Assets/01.Scripts/UI/Scenes/LoginUI.cs
@@ -5,7 +5,7 @@ public class LoginUI : BodyUI
         LoadingTaskManager.Instance.AddTask(GPGSHelper.Init, "구글 플레이 서비스 초기화...");
         LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
         LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
-        LoadingTaskManager.Instance.NextSceneName = "MainScene";
-        MoveScene("LoadingScene");
+        LoadingTaskManager.Instance.NextSceneName = SceneNames.MainScene;
+        MoveScene(SceneNames.LoadingScene);
     }
 }
