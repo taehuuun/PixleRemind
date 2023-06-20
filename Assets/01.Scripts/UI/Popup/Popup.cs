7de5f8166277b7f475883bb7474a0870b37e6853
LimTaeHun <lsw463@naver.com>
2023-06-20 14:49:33 +0900

    Modify: 기존 Popup 클래스의 구조 수정

--- Assets/01.Scripts/UI/Popup/Popup.cs
+++ Assets/01.Scripts/UI/Popup/Popup.cs
@@ -1,13 +1,56 @@
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
-public class Popup : CloseAbleUI
+public class Popup : MonoBehaviour
 {
-    public virtual void Open()
+    public TMP_Text titleText;
+    public TMP_Text bodyText;
+    public Button closeButton;
+
+    public Transform buttonContainer;
+    public Button buttonPrefab;
+
+    private List<Button> _addedButtons = new List<Button>();
+
+    public void SetTitle(string title)
+    {
+        titleText.text = title;
+    }
+
+    public void SetBody(string body)
+    {
+        bodyText.text = body;
+    }
+
+    public void AddButton(string text, UnityAction action)
+    {
+        Button newButton = Instantiate(buttonPrefab, buttonContainer);
+        newButton.GetComponentInChildren<Text>().text = text;
+        newButton.onClick.AddListener(action);
+        
+        _addedButtons.Add(newButton);
+    }
+
+    public void ClearAddedButtons()
+    {
+        foreach (var button in _addedButtons)
+        {
+            Destroy(button.gameObject);
+        }
+        
+        _addedButtons.Clear();
+    }
+
+    public void Show()
     {
         gameObject.SetActive(true);
     }
 
-    public virtual void Close()
+    public void Hide()
     {
         gameObject.SetActive(false);
     }
