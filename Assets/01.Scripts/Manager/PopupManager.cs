ddcc3ad8ae5d9de056718ee57f2faeea74a53dc2
LimTaeHun <lsw463@naver.com>
2023-06-20 15:03:35 +0900

    Feat: PopupManager 기능 구현

--- Assets/01.Scripts/Manager/PopupManager.cs
+++ Assets/01.Scripts/Manager/PopupManager.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class PopupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PopupManager Instance;
+    public Popup popupPrefab;
+
+    private Stack<Popup> popupStack = new Stack<Popup>();
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Backspace))
+        {
+            ClosePopup();
+        }
+    }
+
+    public void ShowPopup(string title, string body)
+    {
+        var newPopup = Instantiate(popupPrefab);
+        newPopup.SetTitle(title);
+        newPopup.SetBody(body);
+        newPopup.Show();
+        popupStack.Push(newPopup);
+    }
+
+    public void ClosePopup()
+    {
+        if (popupStack.Count > 0)
+        {
+            var topPopup = popupStack.Pop();
+            Destroy(topPopup.gameObject);
+        }
+    }
+
+    public void AddButtonToCurrentPopup(string buttonText, UnityAction action)
+    {
+        if (popupStack.Count > 0)
+        {
+            var topPopup = popupStack.Peek();
+            topPopup.AddButton(buttonText, action);
+        }
+    }
+    
+    public bool IsPopupOpen()
+    {
+        return popupStack.Count > 0;
     }
 }
