26fd57504b379376f2d91e7d3a3ce9b50be46f4b
LimTaeHun <lsw463@naver.com>
2023-06-20 18:04:56 +0900

    Add: PlayScene 씬네임 상수 문자열 추가

--- Assets/01.Scripts/Config/SceneNames.cs
+++ Assets/01.Scripts/Config/SceneNames.cs
@@ -4,4 +4,5 @@ public static class SceneNames
     public const string LoadingScene = "01.LoadingScene";
     public const string MainScene = "02.MainScene";
     public const string GalleryScene = "03.GalleryScene";
+    public const string PlayScene = "04.PlayScene";
 }
