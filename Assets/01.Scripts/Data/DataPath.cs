56641c6ba74fe7cdf6a7264c5ba981fcb898ff5b
LimTaeHun <lsw463@naver.com>
2023-06-21 16:56:10 +0900

    Add: DataPath 코드 설명 주석 추가

--- Assets/01.Scripts/Data/DataPath.cs
+++ Assets/01.Scripts/Data/DataPath.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class DataPath : MonoBehaviour
 {
+    // 로컬 저장 베이스 경로
     private static readonly string BasePath = Application.persistentDataPath;
+    
+    // 로컬 토픽 데이터 저장 경로
     public static readonly string GalleryDataPath = Path.Combine(BasePath, "Gallery", "Topics");
 }
