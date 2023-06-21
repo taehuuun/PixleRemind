33383d4f07a04e2cb1ea5d0c7a52268d6f042dbb
LimTaeHun <lsw463@naver.com>
2023-06-21 18:50:45 +0900

    Add: UIHelper, FormatSecondsToTimeString 메서드 코드 설명 추가

--- Assets/01.Scripts/Helpers/UIHelper.cs
+++ Assets/01.Scripts/Helpers/UIHelper.cs
@@ -4,6 +4,11 @@ using UnityEngine.UI;
 
 public static class UIHelper
 {
+    /// <summary>
+    /// 초를 문자열 시분초 형태로 출력 60 -> "01:00"
+    /// </summary>
+    /// <param name="totalSeconds">초 값</param>
+    /// <returns>시분초 형태의 문자열</returns>
     public static string FormatSecondsToTimeString(int totalSeconds)
     {
         int hours = totalSeconds / 3600;
