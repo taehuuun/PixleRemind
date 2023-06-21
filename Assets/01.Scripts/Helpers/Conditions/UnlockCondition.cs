8ffaa96a16e545fe6816684ffc22a3a4b1f96088
LimTaeHun <lsw463@naver.com>
2023-06-21 17:07:41 +0900

    Add: UnlockCondition 코드 설명 주석 추가

--- Assets/01.Scripts/Helpers/Conditions/UnlockCondition.cs
+++ Assets/01.Scripts/Helpers/Conditions/UnlockCondition.cs
@@ -4,7 +4,10 @@ using Firebase.Firestore;
 [FirestoreData, Serializable]
 public class UnlockCondition
 {
+    // 잠금 해제 타입
     [FirestoreProperty] public UnlockType Type { get; set; }
+    
+    // 타입별 해제 조건 
     [FirestoreProperty] public int Count { get; set; }
 
     public UnlockCondition()
