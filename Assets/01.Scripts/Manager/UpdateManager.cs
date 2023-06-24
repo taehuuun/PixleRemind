d0df041cb2e7825a496942dd0e4f9f024151a440
LimTaeHun <lsw463@naver.com>
2023-06-24 23:12:53 +0900

    Add: UpdateManager 코드 주석 설명 추가

--- Assets/01.Scripts/Manager/UpdateManager.cs
+++ Assets/01.Scripts/Manager/UpdateManager.cs
@@ -9,20 +9,24 @@ public class UpdateManager : MonoBehaviour
 {
     public UpdatePopup updatePopup;
 
+    // 다운로드가 완료 되었을 때 이벤트
     public event Action OnDownloadCompleted;
-    public event Action<Exception> OnDownloadFailed; 
+    
+    // 다운로드가 실패 했을 때 이벤트
+    public event Action<Exception> OnDownloadFailed;
+    
+    // 업데이트 체크가 완료 되었을 때 이벤트
     public event Action<bool> OnUpdateCheckCompleted;
     
     private List<TopicData> _topicDataList;
 
     private async void Start()
     {
-        Debug.Log("UpdateManager Start");
+        // Firestore에 있는 모든 토픽 데이터를 로드
         _topicDataList =
             await FirebaseManager.ins.Firestore.GetAllData<TopicData>(FirestoreCollections.GalleryData);
 
-        Debug.Log(_topicDataList.Count);
-
+        // 로드된 토픽 데이터 리스트를 정렬
         _topicDataList.Sort(new TopicDataListComparer());
 
         if (DataManager.Instance.userData.LocalTopicDataIDs == null)
@@ -33,30 +37,36 @@ public class UpdateManager : MonoBehaviour
         await CheckForUpdated();
     }
 
+    /// <summary>
+    /// 업데이트 체크 메서드
+    /// </summary>
+    /// <returns></returns>
     public Task CheckForUpdated()
     {
-        Debug.Log("UpdateManager CheckForUpdated");
+        // 로컬에 저장되어 있는 토픽 데이터 ID 리스트
         List<string> localTopicDataIDs = DataManager.Instance.userData.LocalTopicDataIDs;
-        List<string> missingDataIDs = new List<string>();
-
+        
+        // 다운로드 하지 않은 토픽 데이터 리스트
         List<TopicData> missingDataList = new List<TopicData>();
+        
+        // 추가된 토픽 데이터 리스트
         List<TopicData> outdatedDataList = new List<TopicData>();
 
+        // 토컬 ID 리스트가 비었거나, 추가된 ID가 없는 경우 
         if (localTopicDataIDs == null || localTopicDataIDs.Count == 0)
         {
-            Debug.Log("로컬 데이터가 없음");
+            // Firestore에서 로드한 데이터를 missingDataList에 추가
             foreach (var topicData in _topicDataList)
             {
-                missingDataIDs.Add(topicData.Title);
                 missingDataList.Add(topicData);
             }
-
-            updatePopup.Show(outdatedDataList, missingDataList);
         }
         else
         {
-            missingDataIDs = _topicDataList.Select(data => data.ID).Except(localTopicDataIDs).ToList();
-
+            // 다운로드 하지 않은 토픽 데이터 ID 리스트
+            List<string> missingDataIDs = _topicDataList.Select(data => data.ID).Except(localTopicDataIDs).ToList();
+            
+            // 미 다운로드 ID에 해당하는 Firestore 데이터를 missingDataList에 추가
             foreach (var id in missingDataIDs)
             {
                 TopicData serverData = _topicDataList.Find(data => data.ID == id);
@@ -67,6 +77,7 @@ public class UpdateManager : MonoBehaviour
                 }
             }
             
+            // Firestore 데이터와 비교하여 로컬에 없는 데이터를 outdatedDataList 추가
             foreach (var id in localTopicDataIDs)
             {
                 TopicData localData = DataManager.LoadJsonData<TopicData>(DataPath.GalleryDataPath, id);
@@ -78,10 +89,8 @@ public class UpdateManager : MonoBehaviour
                 }
             }
         }
-
-        Debug.Log($"Missing Data Count: {missingDataList.Count}");
-        Debug.Log($"Outdated Data Count : {outdatedDataList.Count}");
-
+        
+        // 각 DataList에 데이터들이 존재 할 경우 UpdatePopup을 화면에 띄움
         bool updatedPopupShow = missingDataList.Count > 0 || outdatedDataList.Count > 0;
         
         if (updatedPopupShow)
@@ -89,14 +98,18 @@ public class UpdateManager : MonoBehaviour
             updatePopup.Show(outdatedDataList, missingDataList);
         }
         
+        // 업데이트 체크 완료 이벤트 실행
         OnUpdateCheckCompleted?.Invoke(updatedPopupShow);
         
         return Task.CompletedTask;
     }
 
+    /// <summary>
+    /// ID와 일치하는 토픽 데이터를 다운로드 하는 메서드
+    /// </summary>
+    /// <param name="topicID">다운로드 할 Topic ID</param>
     public async Task DownloadTopicData(string topicID)
     {
-        Debug.Log("UpdateManager DownloadTopicData");
         TopicData serverData = _topicDataList.Find(data => data.ID == topicID);
 
         try
