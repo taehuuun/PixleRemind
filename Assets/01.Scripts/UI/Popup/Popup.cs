e3e2f11794d2f8cc33e14cc3c7ea32a928452b28
LimTaeHun <lsw463@naver.com>
2023-06-24 22:38:00 +0900

    Add: Popup 코드 주석 설명 추가

--- Assets/01.Scripts/UI/Popup/Popup.cs
+++ Assets/01.Scripts/UI/Popup/Popup.cs
@@ -16,16 +16,29 @@ public class Popup : MonoBehaviour
 
     private List<Button> _addedButtons = new List<Button>();
 
+    /// <summary>
+    /// 팝업의 타이틀 텍스트 설정 메서드
+    /// </summary>
+    /// <param name="title">변경할 텍스트</param>
     public void SetTitle(string title)
     {
         titleText.text = title;
     }
-
+    
+    /// <summary>
+    /// 본문 내용을 설정하는 메서드
+    /// </summary>
+    /// <param name="body">본문 내용</param>
     public void SetBody(string body)
     {
         bodyText.text = body;
     }
 
+    /// <summary>
+    /// 팝업에 버튼을 추가하는 메서드
+    /// </summary>
+    /// <param name="text">추가할 버튼의 텍스트</param>
+    /// <param name="action">추가할 버턴의 이벤트</param>
     public void AddButton(string text, UnityAction action)
     {
         Button newButton = Instantiate(buttonPrefab, buttonContainer);
@@ -34,7 +47,10 @@ public class Popup : MonoBehaviour
         
         _addedButtons.Add(newButton);
     }
-
+    
+    /// <summary>
+    /// 추가한 버튼들을 제거하는 메서드
+    /// </summary>
     public void ClearAddedButtons()
     {
         foreach (var button in _addedButtons)
@@ -45,11 +61,17 @@ public class Popup : MonoBehaviour
         _addedButtons.Clear();
     }
 
+    /// <summary>
+    /// 팝업을 화면에 띄우는 메서드
+    /// </summary>
     public void Show()
     {
         gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// 팝업을 화면에서 숨기는 메서드
+    /// </summary>
     public void Hide()
     {
         gameObject.SetActive(false);
