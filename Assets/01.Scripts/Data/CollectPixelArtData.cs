1d17decd217895f3b70b01d58df8a1d529260566
LimTaeHun <lsw463@naver.com>
2023-07-04 23:47:07 +0900

    Add: CollectPixelArtData의 프로퍼티 추가

--- Assets/01.Scripts/Data/CollectPixelArtData.cs
+++ Assets/01.Scripts/Data/CollectPixelArtData.cs
@@ -1,5 +1,12 @@
+using System;
+using Firebase.Firestore;
 using UnityEngine;
 
+[FirestoreData, Serializable]
 public class CollectPixelArtData : MonoBehaviour
 {
+    [FirestoreProperty] public string Title { get; set; }
+    [FirestoreProperty] public string Description { get; set; }
+    [FirestoreProperty] public string ThumbnailData { get; set; }
+    [FirestoreProperty] public int PlayTime { get; set; }
 }
