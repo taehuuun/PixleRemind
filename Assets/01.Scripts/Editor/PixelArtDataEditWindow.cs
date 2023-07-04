d54ab3f5f4236aa1f12511f90d9eb2b10e948881
LimTaeHun <lsw463@naver.com>
2023-07-04 23:30:04 +0900

    Feat: PixelArtData의 Description 프로퍼티 추가에 따른 커스텀 에디터 관련 코드 수정

--- Assets/01.Scripts/Editor/PixelArtDataEditWindow.cs
+++ Assets/01.Scripts/Editor/PixelArtDataEditWindow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PixelArtDataEditWindow : EditorWindow
 {
     private string _tmpTitleID;
+    private string _tmpDescription;
     private Texture2D _tmpPixelArt;
     private Difficulty _tmpDifficulty;
     private Action<PixelArtData> _onConfirm;
@@ -19,15 +20,18 @@ public class PixelArtDataEditWindow : EditorWindow
 
     private void OnGUI()
     {
-        _tmpTitleID = EditorGUILayout.TextField("TitleID", _tmpTitleID);
+        _tmpTitleID = EditorGUILayout.TextField("Title", _tmpTitleID);
         _tmpPixelArt = (Texture2D)EditorGUILayout.ObjectField("Pixel Art", _tmpPixelArt, typeof(Texture2D));
         _tmpDifficulty = (Difficulty)EditorGUILayout.EnumPopup("Difficulty", _tmpDifficulty);
+        
+        EditorGUILayout.LabelField("Description");
+        _tmpDescription = EditorGUILayout.TextArea(_tmpDescription, GUILayout.Height(100));
 
         if (GUILayout.Button("Confirm"))
         {
             if (_tmpPixelArt != null && !string.IsNullOrEmpty(_tmpTitleID))
             {
-                var newPixelArtData = PixelArtHelper.ExportPixelData(_tmpTitleID, _tmpPixelArt, _tmpDifficulty);
+                var newPixelArtData = PixelArtHelper.ExportPixelData(_tmpTitleID, _tmpDescription, _tmpPixelArt, _tmpDifficulty);
                 _onConfirm?.Invoke(newPixelArtData);
                 Close();
             }
