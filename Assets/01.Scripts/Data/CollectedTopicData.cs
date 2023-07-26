3337e2aa3fdec3d3f3415874068aca5d68fda1b6
LimTaeHun <lsw463@naver.com>
2023-07-26 13:56:26 +0900

    Modify: 프로퍼티 제거 및 추가, 생성자 수정
    ThumbnailData 프로퍼티 제거, TotalPixelArtDataCount, CollectedPixelArtDataList 프로퍼티 추가

--- Assets/01.Scripts/Data/CollectedTopicData.cs
+++ Assets/01.Scripts/Data/CollectedTopicData.cs
@@ -1,17 +1,20 @@
 using System;
+using System.Collections.Generic;
 using Firebase.Firestore;
 
 [FirestoreData, Serializable]
 public class CollectedTopicData
 {
     [FirestoreProperty] public string Title { get; private set; }
-    [FirestoreProperty] public string ThumbnailData { get; private set; }
-    [FirestoreProperty] public string Description { get; private set; }
+    [FirestoreProperty] public string Description  { get; private set; }
+    [FirestoreProperty] public int TotalPixelArtDataCount { get; private set; }
+    [FirestoreProperty] public List<CollectedPixelArtData> CollectedPixelArtDataList { get; private set; }
 
-    public CollectedTopicData(string title, string thumbnailData, string description)
+    public CollectedTopicData(string title, string description, int totalPixelArtDataCount)
     {
         Title = title;
-        ThumbnailData = thumbnailData;
         Description = description;
+        TotalPixelArtDataCount = totalPixelArtDataCount;
+        CollectedPixelArtDataList = new List<CollectedPixelArtData>();
     }
 }
