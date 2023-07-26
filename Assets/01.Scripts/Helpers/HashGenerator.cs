fd713dda2977039017fe91e2ee546c42a2cb4a4d
LimTaeHun <lsw463@naver.com>
2023-07-26 19:50:09 +0900

    Feat: GeneratorHash 메서드 구현

--- Assets/01.Scripts/Helpers/HashGenerator.cs
+++ Assets/01.Scripts/Helpers/HashGenerator.cs
@@ -1,4 +1,22 @@
+using System.Security.Cryptography;
+using System.Text;
+
 public static class HashGenerator
 {
-    
+    public static string GeneratorHash(string input)
+    {
+        using (SHA256 sha256 = SHA256.Create(input))
+        {
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
 }
