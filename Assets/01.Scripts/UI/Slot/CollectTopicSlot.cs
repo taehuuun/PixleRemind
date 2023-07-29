1a70b420808ab331e3d747960461f1e2b2225171
LimTaeHun <lsw463@naver.com>
2023-07-29 22:36:21 +0900

    Remove: CollectTopicSlot 클래스의 collectPixelArtSlotPrefab 필드 제거 및 CollectPixelArtSlot 생성 코드 제거

--- Assets/01.Scripts/UI/Slot/CollectTopicSlot.cs
+++ Assets/01.Scripts/UI/Slot/CollectTopicSlot.cs
@@ -5,16 +5,13 @@ public class CollectTopicSlot : MonoBehaviour
 {
     [SerializeField] private TMP_Text titleText;
     [SerializeField] private Transform genPixelArtSlotParent;
-    [SerializeField] private CollectPixelArtSlot collectPixelArtSlotPrefab;
 
     public void SetSlot(CollectedTopicData collectedTopicData)
     {
         titleText.text = $"{collectedTopicData.Title} ({collectedTopicData.CollectedPixelArtDataList.Count} / {collectedTopicData.TotalPixelArtDataCount})";
 
-        for (int i = 0; i < collectedTopicData.CollectedPixelArtDataList.Count; i++)
-        {
-            CollectPixelArtSlot newCollectPixelArtSlot = Instantiate(collectPixelArtSlotPrefab, genPixelArtSlotParent);
-            newCollectPixelArtSlot.SetSlot(collectedTopicData.CollectedPixelArtDataList[i]);
-        } 
+    public Transform GetCollectPixelArtParent()
+    {
+        return genPixelArtSlotParent;
     }
 }
