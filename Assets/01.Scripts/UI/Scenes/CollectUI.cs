04a1f82e422c2f5fdef3381ecea567aa63190bb2
LimTaeHun <lsw463@naver.com>
2023-07-29 20:04:33 +0900

    Feat: SetSlot 메서드 구현
    도감 씬 진입 시 수집 토픽 슬롯을 생성 하는 기능 및 관련 필드 선언

--- Assets/01.Scripts/UI/Scenes/CollectUI.cs
+++ Assets/01.Scripts/UI/Scenes/CollectUI.cs
@@ -1,8 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectUI : BodyUI
 {
+    [SerializeField] private Transform genCollectTopicSlotParent;
+    [SerializeField] private CollectTopicSlot collectTopicSlotPrefab;
+    
+    private void Start()
+    {
+        SetSlot();
+    }
+    
+    private void SetSlot()
+    {
+        List<CollectedTopicData> curCollectTopicDataList = DataManager.userData.CollectedTopicDataList;
 
+        foreach (CollectedTopicData collectedTopicData in curCollectTopicDataList)
+        {
+            CollectTopicSlot newCollectTopicSlot = Instantiate(collectTopicSlotPrefab, genCollectTopicSlotParent);
+            newCollectTopicSlot.SetSlot(collectedTopicData);
+        }
+    }
 }
