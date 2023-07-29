6075e8fa2934536f40fcc9528413def0348be362
LimTaeHun <lsw463@naver.com>
2023-07-29 21:23:44 +0900

    Add: ID 프로퍼티 추가
    GameManager에서 플레이한 픽셀 아트를 수집 처리할 때 해당 픽셀 아트가 속해야할 CollectedTopicData를 찾기위한 ID 프로퍼티 추가

--- Assets/01.Scripts/Data/CollectedTopicData.cs
+++ Assets/01.Scripts/Data/CollectedTopicData.cs
@@ -5,6 +5,7 @@ using Firebase.Firestore;
 [FirestoreData, Serializable]
 public class CollectedTopicData
 {
+    [FirestoreProperty] public string ID { get; private set; }
     [FirestoreProperty] public string Title { get; private set; }
     [FirestoreProperty] public string Description { get; private set; }
     [FirestoreProperty] public int TotalPixelArtDataCount { get; private set; }
@@ -12,14 +13,16 @@ public class CollectedTopicData
 
     public CollectedTopicData()
     {
+        ID = "";
         Title = "";
         Description = "";
         TotalPixelArtDataCount = 0;
         CollectedPixelArtDataList = new List<CollectedPixelArtData>();
     }
 
-public CollectedTopicData(string title, string description, int totalPixelArtDataCount)
+public CollectedTopicData(string id, string title, string description, int totalPixelArtDataCount)
     {
+        ID = id;
         Title = title;
         Description = description;
         TotalPixelArtDataCount = totalPixelArtDataCount;
