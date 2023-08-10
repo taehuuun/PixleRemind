340291501ee2ae7fea86df4a54a2ce4735444be9
taehuuun <lsw463@naver.com>
2023-08-11 02:37:06 +0900

    Add: BaseThumbnailData 클래스 내 프로퍼티 추가
    ThumbnailData, ThumbnailSize 추가

--- Assets/01.Scripts/Data/BaseThumbnailData.cs
+++ Assets/01.Scripts/Data/BaseThumbnailData.cs
@@ -1,3 +1,9 @@
+using System;
+using Firebase.Firestore;
+
+[FirestoreData, Serializable]
 public class BaseThumbnailData : BaseData
 {
+    [FirestoreProperty] public string ThumbnailData { get; set; }
+    [FirestoreProperty] public int ThumbnailSize { get; set; }
 }
