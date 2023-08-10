e30ac93309da8e7056b7ab8029bea90d4b9cb991
taehuuun <lsw463@naver.com>
2023-08-11 02:41:29 +0900

    Feat: BaseThumbnailData클래스 내 생성자 구현

--- Assets/01.Scripts/Data/BaseThumbnailData.cs
+++ Assets/01.Scripts/Data/BaseThumbnailData.cs
@@ -6,4 +6,17 @@ public class BaseThumbnailData : BaseData
 {
     [FirestoreProperty] public string ThumbnailData { get; set; }
     [FirestoreProperty] public int ThumbnailSize { get; set; }
+
+    public BaseThumbnailData()
+    {
+        ThumbnailData = string.Empty;
+        ThumbnailSize = 0;
+    }
+
+    public BaseThumbnailData(string id, string title, string description, string thumbnailData, int thumbnailSize) 
+        : base(id, title, description)
+    {
+        ThumbnailData = thumbnailData;
+        ThumbnailSize = thumbnailSize;
+    }
 }
