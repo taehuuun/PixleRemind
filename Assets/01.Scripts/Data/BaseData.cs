f1cc470c3256f3a5000862aeac62e7ca179226be
taehuuun <lsw463@naver.com>
2023-08-11 01:21:00 +0900

    Feat: BaseData 클래스 내 생성자 메서드 구현

--- Assets/01.Scripts/Data/BaseData.cs
+++ Assets/01.Scripts/Data/BaseData.cs
@@ -5,4 +5,18 @@ public class BaseData
     [FirestoreProperty] public string ID { get; set; }
     [FirestoreProperty] public string Title { get; set; }
     [FirestoreProperty] public string Description { get; set; }
+    
+    public BaseData()
+    {
+        ID = string.Empty;
+        Title = string.Empty;
+        Description = string.Empty;
+    }
+    
+    public BaseData(string id, string title, string description)
+    {
+        ID = id;
+        Title = title;
+        Description = description;
+    }
 }
