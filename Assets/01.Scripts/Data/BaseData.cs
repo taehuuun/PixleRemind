477989daea8812490b19d74ee967a43c2fec8f6f
taehuuun <lsw463@naver.com>
2023-08-11 01:20:21 +0900

    Add: BaseData 클래스에 ID, Title, Description 프로퍼티 추가

--- Assets/01.Scripts/Data/BaseData.cs
+++ Assets/01.Scripts/Data/BaseData.cs
@@ -1,5 +1,8 @@
-using UnityEngine;
+using Firebase.Firestore;
 
 public class BaseData
 {
+    [FirestoreProperty] public string ID { get; set; }
+    [FirestoreProperty] public string Title { get; set; }
+    [FirestoreProperty] public string Description { get; set; }
 }
