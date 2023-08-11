35d703196aceab743d1d980a083ef8b2da300b26
taehuuun <lsw463@naver.com>
2023-08-11 13:40:46 +0900

    Refactor: DownloadTopicData 클래스를 BaseData 클래스의 자식 클래스로 개선

--- Assets/01.Scripts/Data/DownloadTopicData.cs
+++ Assets/01.Scripts/Data/DownloadTopicData.cs
@@ -3,28 +3,19 @@ using System.Collections.Generic;
 using Firebase.Firestore;
 
 [FirestoreData, Serializable]
-public class DownloadTopicData
+public class DownloadTopicData : BaseData
 {
-    [FirestoreProperty] public string ID { get; private set; }
-    [FirestoreProperty] public string Title { get; private set; }
-    [FirestoreProperty] public string Description { get; private set; }
     [FirestoreProperty] public int TotalPixelArtDataCount { get; private set; }
     [FirestoreProperty] public List<CollectedPixelArtData> CollectedPixelArtDataList { get; private set; }
 
     public DownloadTopicData()
     {
-        ID = "";
-        Title = "";
-        Description = "";
         TotalPixelArtDataCount = 0;
         CollectedPixelArtDataList = new List<CollectedPixelArtData>();
     }
 
-    public DownloadTopicData(string id, string title, string description, int totalPixelArtDataCount)
+    public DownloadTopicData(string id, string title, string description, int totalPixelArtDataCount) : base(id, title, description)
     {
-        ID = id;
-        Title = title;
-        Description = description;
         TotalPixelArtDataCount = totalPixelArtDataCount;
         CollectedPixelArtDataList = new List<CollectedPixelArtData>();
     }
