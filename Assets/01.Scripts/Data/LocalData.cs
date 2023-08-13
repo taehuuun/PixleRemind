5c2c4e19189d1bdbddf9c9d764646586e9d9714a
taehuuun <lsw463@naver.com>
2023-08-13 15:37:46 +0900

    Feat: LocalData 클래스에 프로퍼티 추가

--- Assets/01.Scripts/Data/LocalData.cs
+++ Assets/01.Scripts/Data/LocalData.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+
 public class LocalData
 {
+    public Dictionary<string, TopicData> LocalTopicData { get; set; }
+    public string LastSelectID { get; set; }
 }
