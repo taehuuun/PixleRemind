ff4f848cfcc411b57c8b249a1dc30a942c3c9986
taehuuun <lsw463@naver.com>
2023-08-13 18:01:27 +0900

    Refactor: UserData 클래스의 프로퍼티 정리 및 간소화
    LocalTopicDataIDs, SelectTopicID, SelectPixelArtID 프로퍼티를 제거하고, 생성자에서 LastUpdated 필드의 초기화를 추가

--- Assets/01.Scripts/Data/UserData.cs
+++ Assets/01.Scripts/Data/UserData.cs
@@ -5,15 +5,12 @@ using Firebase.Firestore;
 [FirestoreData, Serializable]
 public class UserData
 {
-    [FirestoreProperty] public List<string> LocalTopicDataIDs { get; set; }
     [FirestoreProperty] public List<DownloadTopicData> DownloadTopicDataList { get; set; }
     [FirestoreProperty] public DateTime LastUpdated { get; set; }
-    [FirestoreProperty] public string SelectTopicID { get; set; } 
-    [FirestoreProperty] public string SelectPixelArtID { get; set; }
 
     public UserData()
     {
-        LocalTopicDataIDs = new List<string>();
         DownloadTopicDataList = new List<DownloadTopicData>();
+        LastUpdated = new DateTime();
     }
 }
