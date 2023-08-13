0dfce6fd823cba63e4422e3b695661905572d548
taehuuun <lsw463@naver.com>
2023-08-13 17:57:30 +0900

    Refactor: LocalData 클래스의 마지막 선택 ID 관리 방식 변경
    _lastSelectID 필드를 추가하고 이를 활용하여 마지막 선택된 토픽 ID와 픽셀 아트 ID를 개별적으로 가져올 수 있는 메서드를 구현

--- Assets/01.Scripts/Data/LocalData.cs
+++ Assets/01.Scripts/Data/LocalData.cs
@@ -1,7 +1,28 @@
+using System;
 using System.Collections.Generic;
 
 public class LocalData
 {
     public Dictionary<string, TopicData> LocalTopicData { get; set; }
-    public string LastSelectID { get; set; }
+    private string _lastSelectID;
+
+    private string[] SplitLastSelectID()
+    {
+        if (string.IsNullOrEmpty(_lastSelectID) || _lastSelectID.Length <= 1)
+            return Array.Empty<string>();
+
+        return _lastSelectID.Split('/');
+    }
+
+    public string GetLastSelectTopicID()
+    {
+        var splitSelectID = SplitLastSelectID();
+        return splitSelectID.Length > 0 ? splitSelectID[0] : string.Empty;
+    }
+
+    public string GetLastSelectPixelArtID()
+    {
+        var splitSelectID = SplitLastSelectID();
+        return splitSelectID.Length > 1 ? splitSelectID[1] : string.Empty;
+    }
 }
