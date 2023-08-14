237a2f129b52b729862d797b11c9083ccb170bc6
taehuuun <lsw463@naver.com>
2023-08-15 02:48:54 +0900

    Refactor: DataSyncManager 구현에 따른 기존 LoginManager의 LoadUserData 메서드 수정

--- Assets/01.Scripts/Manager/LoginManager.cs
+++ Assets/01.Scripts/Manager/LoginManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -45,21 +46,7 @@ public class LoginManager : MonoBehaviour
 #else
             string FUID = "Test";
 #endif
-            bool userDataExists =
-                await FirebaseManager.ins.Firestore.CheckDocumentExists(FirestoreCollections.UserData, FUID);
-            
-            // Firestore의 FUID에 해당하는 UserData와 LocalData가 존재하는지 체크
-            // 둘 다 존재 할경우)
-            //      두 데이터를 비교하여 UserData에 존재 하지만, LocalData에 존재하지 않는 데이터를 찾아 
-            //      LocalData를 UserData와 일치 시킴
-            // UserData만 존재 할 경우)
-            //      LocalData의 내용을 UserData와 일치 시킴
-            // LocalData만 존재 하는 경우
-            //      UserData가 존재하지 않는 경우 해당 FUID의 데이터가 없음 (플레이 한 적이 없음) 하지만 LocalData가 존재 할 때 
-            //      다른 플레이어의 데이터를 경로에 배치 시킨걸로 간주 => 모든 데이터 리셋
-            // 둘다 존재 하지 않는 경우
-            //      UserData와 LocalData를 새로 생성 후 UserData를 Firestore에 업로드, LocalData는 로컬 경로에 저장
-            
+            await DataSyncManager.SyncUserDataAndLocalData(FUID);
         }
         catch (Exception ex)
         {
