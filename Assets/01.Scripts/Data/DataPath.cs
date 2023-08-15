e2fc8e5e4f0904bd3e50032fd0c78f07618d55b8
taehuuun <lsw463@naver.com>
2023-08-15 13:57:51 +0900

    Remove: LocalData 구조 변경에 따른 LocalTopicData 경로 필드 제거

--- Assets/01.Scripts/Data/DataPath.cs
+++ Assets/01.Scripts/Data/DataPath.cs
@@ -7,6 +7,5 @@ public static class DataPath
     private static readonly string BasePath = Application.persistentDataPath;
     
     // 로컬 토픽 데이터 저장 경로
-    public static readonly string LocalTopicData = Path.Combine(BasePath, "Topics");
     public static readonly string LocalData = Path.Combine(BasePath,"LocalData");
 }
