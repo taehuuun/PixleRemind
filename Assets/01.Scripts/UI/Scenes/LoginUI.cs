1f8aad37e044fa2476fddf8666ac85f62f87e5b9
taehuuun <lsw463@naver.com>
2023-08-15 23:13:21 +0900

    Refactor: LoginUI내 Start 메서드 수정
    로그인 후 데이터 로드, 비교, 동기화 작업을 담당하는 DataSyncmanager 구현에 따라 로딩 작업간 진행 해야 할 Task 및 메시지 수정

--- Assets/01.Scripts/UI/Scenes/LoginUI.cs
+++ Assets/01.Scripts/UI/Scenes/LoginUI.cs
@@ -4,7 +4,7 @@ public class LoginUI : BodyUI
     {
         LoadingTaskManager.Instance.AddTask(GPGSHelper.Init, "구글 플레이 서비스 초기화...");
         LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
-        LoadingTaskManager.Instance.AddTask(LoginManager.LoadUserData, "유저 데이터 가져오는 중...");
+        LoadingTaskManager.Instance.AddTask(DataSyncManager.SyncUserDataAndLocalData, "유저 / 로컬 데이터 동기화 중...");
         LoadingTaskManager.Instance.NextSceneName = SceneNames.MainScene;
         MoveScene(SceneNames.LoadingScene);
     }
