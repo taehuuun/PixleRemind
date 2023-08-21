6c65ad1a4584dbeebf2030c290fef771ac8e1e13
taehuuun <lsw463@naver.com>
2023-08-22 07:06:17 +0900

    Refactor: CollectedPixelArtData내 빈 생성자 메서드를 체이닝

--- Assets/01.Scripts/Data/CollectedPixelArtData.cs
+++ Assets/01.Scripts/Data/CollectedPixelArtData.cs
@@ -6,10 +6,7 @@ public class CollectedPixelArtData : BaseThumbnailData
 {
     [FirestoreProperty] public int PlayTime { get; private set; }
 
-    public CollectedPixelArtData()
-    {
-        PlayTime = 0;
-    }
+    public CollectedPixelArtData() : this(string.Empty, string.Empty, string.Empty, string.Empty, 0, 0){}
 
     public CollectedPixelArtData(string id, string title, string description, string thumbnailData, int thumbnailSize,
         int playTime)
