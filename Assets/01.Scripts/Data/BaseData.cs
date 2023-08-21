29e1be1b7e23579f96196371585eafda4b7b5499
taehuuun <lsw463@naver.com>
2023-08-22 04:36:55 +0900

    Refactor: BaseData 빈 매개변수 생성자 체인화

--- Assets/01.Scripts/Data/BaseData.cs
+++ Assets/01.Scripts/Data/BaseData.cs
@@ -8,12 +8,7 @@ public class BaseData
     [FirestoreProperty] public string Title { get; private set; }
     [FirestoreProperty] public string Description { get; private set; }
     
-    public BaseData()
-    {
-        ID = string.Empty;
-        Title = string.Empty;
-        Description = string.Empty;
-    }
+    public BaseData() : this(string.Empty, string.Empty,string.Empty) { }
     
     public BaseData(string id, string title, string description)
     {
