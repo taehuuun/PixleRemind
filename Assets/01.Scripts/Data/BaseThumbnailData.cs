c582b24a601111c341e12602a2360a9edc60e615
taehuuun <lsw463@naver.com>
2023-08-22 06:37:44 +0900

    Refactor: BaseThumbnailData 클래스 빈 생성자 메서드를 생성자 체이닝을 통해 개선

--- Assets/01.Scripts/Data/BaseThumbnailData.cs
+++ Assets/01.Scripts/Data/BaseThumbnailData.cs
@@ -7,11 +7,7 @@ public class BaseThumbnailData : BaseData
     [FirestoreProperty] public string ThumbnailData { get; private set; }
     [FirestoreProperty] public int ThumbnailSize { get; private set; }
 
-    public BaseThumbnailData()
-    {
-        ThumbnailData = string.Empty;
-        ThumbnailSize = 0;
-    }
+    public BaseThumbnailData() : this(string.Empty, string.Empty, string.Empty, string.Empty, 0) { }
 
     public BaseThumbnailData(string id, string title, string description, string thumbnailData, int thumbnailSize) 
         : base(id, title, description)
