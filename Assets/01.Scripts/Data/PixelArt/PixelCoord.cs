b85a0d781689831023f2168ddfd6e5c54938104f
taehuuun <lsw463@naver.com>
2023-08-23 04:55:26 +0900

    Refactor: PixelCoord 생성자 체이넹

--- Assets/01.Scripts/Data/PixelArt/PixelCoord.cs
+++ Assets/01.Scripts/Data/PixelArt/PixelCoord.cs
@@ -7,12 +7,7 @@ public class PixelCoord
     [FirestoreProperty] public int X { get; private set; }
     [FirestoreProperty] public int Y { get; private set; }
 
-    public PixelCoord()
-    {
-        X = 0;
-        Y = 0;
-    }
-
+    public PixelCoord() : this(0, 0) {}
     public PixelCoord(int x, int y)
     {
         X = x;
