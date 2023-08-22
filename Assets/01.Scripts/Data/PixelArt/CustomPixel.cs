839800e7a32d3dcc55de7daef3c7905ca8ddd706
taehuuun <lsw463@naver.com>
2023-08-23 03:48:16 +0900

    Refactor: CustomPixel 클래스의 미사용 생성자 제거
    - CustomPixel() 제거
    - CustomPixel(CustomRGBA customRgba) 제거

--- Assets/01.Scripts/Data/PixelArt/CustomPixel.cs
+++ Assets/01.Scripts/Data/PixelArt/CustomPixel.cs
@@ -8,22 +8,7 @@ public class CustomPixel
     [FirestoreProperty] public CustomRGBA OriginalColor { get; private set; }
     [FirestoreProperty] public CustomRGBA GrayColor { get; private set; }
     [FirestoreProperty] public List<PixelCoord> PixelCoords { private get; set; }
-
-    public CustomPixel()
-    {
-        OriginalColor = new CustomRGBA(0, 0, 0, 0);
-        GrayColor = new CustomRGBA(0, 0, 0, 0);
-        PixelCoords = new List<PixelCoord>();
-    }
-
-    public CustomPixel(CustomRGBA customRgba)
-    {
-        float grayValue = GetGrayValue(customRgba.R, customRgba.G, customRgba.B);
-
-        OriginalColor = new CustomRGBA(customRgba.R, customRgba.G, customRgba.B, customRgba.A);
-        GrayColor = new CustomRGBA(grayValue, grayValue, grayValue, customRgba.A);
-    }
-
+    
     public CustomPixel(float r, float g, float b, float a)
     {
         float grayValue = GetGrayValue(r, g, b);
