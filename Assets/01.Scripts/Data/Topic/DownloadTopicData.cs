b2da7cc3efd533b7f15676a68e598225a09d76ec
taehuuun <lsw463@naver.com>
2023-08-25 08:16:19 +0900

    Refactor: DownloadTopicData 클래스 내 생성자 체이닝 체이닝 수정

--- Assets/01.Scripts/Data/Topic/DownloadTopicData.cs
+++ Assets/01.Scripts/Data/Topic/DownloadTopicData.cs
@@ -8,12 +8,7 @@ public class DownloadTopicData : BaseData
     [FirestoreProperty] public int TotalPixelArtDataCount { get; private set; }
     [FirestoreProperty] public List<CollectedPixelArtData> CollectedPixelArtDataList { private get; set; }
 
-    public DownloadTopicData()
-    {
-        TotalPixelArtDataCount = 0;
-        CollectedPixelArtDataList = new List<CollectedPixelArtData>();
-    }
-
+    public DownloadTopicData() : this(string.Empty,string.Empty, string.Empty,0) { }
     public DownloadTopicData(string id, string title, string description, int totalPixelArtDataCount) : base(id, title, description)
     {
         TotalPixelArtDataCount = totalPixelArtDataCount;
