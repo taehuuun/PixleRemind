15e02275b12fce5a830c08621efd503e1e47063e
taehuuun <lsw463@naver.com>
2023-08-25 18:51:30 +0900

    Refactor: TopicData 클래스 내 TotalCount 프로퍼티 제거 및 GetPixelArtsCount 메서드 구현
    PixelArtDataList의 Count 프로퍼티를 통해 기존 TotalCount 프로퍼티를 제거 및 리스트의 카운트를 반환하는 메서드 구현

--- Assets/01.Scripts/Data/Topic/TopicData.cs
+++ Assets/01.Scripts/Data/Topic/TopicData.cs
@@ -6,7 +6,6 @@ using Firebase.Firestore;
 public class TopicData : BaseThumbnailData
 {
     [FirestoreProperty] public int CompleteCount { get; private set; }
-    [FirestoreProperty] public int TotalCount { get; private set; }
     [FirestoreProperty] public bool Complete { get; private set; }
     [FirestoreProperty] public bool Updateable { get; private set; }
     [FirestoreProperty] public bool IsLocked { get; private set; }
@@ -14,7 +13,7 @@ public class TopicData : BaseThumbnailData
     [FirestoreProperty] public DateTime LastUpdated { get; private set; }
     [FirestoreProperty] public List<PixelArtData> PixelArtDataList { private get; set; }
 
-    public TopicData() : this(string.Empty, string.Empty,string.Empty,string.Empty,0,0,0,false,false,false,new UnlockCondition(),DateTime.Now,new List<PixelArtData>()) { }
+    public TopicData() : this(string.Empty, string.Empty,string.Empty,string.Empty,0,0,false,false,false,new UnlockCondition(),DateTime.Now,new List<PixelArtData>()) { }
 
     public TopicData(
         string id,
@@ -23,7 +22,6 @@ public class TopicData : BaseThumbnailData
         string thumbnailData,
         int thumbnailSize,
         int completeCount,
-        int totalCount,
         bool complete,
         bool updateable,
         bool isLocked,
@@ -32,12 +30,16 @@ public class TopicData : BaseThumbnailData
         List<PixelArtData> pixelArtDataList) : base(id, title, description, thumbnailData, thumbnailSize)
     {
         CompleteCount = completeCount;
-        TotalCount = totalCount;
         Complete = complete;
         Updateable = updateable;
         IsLocked = isLocked;
         UnlockCondition = unlockCondition;
         LastUpdated = lastUpdated;
         PixelArtDataList = pixelArtDataList;
+    }
+
+    public int GetPixelArtsCount()
+    {
+        return PixelArtDataList.Count;
     }
 }
