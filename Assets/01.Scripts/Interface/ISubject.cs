518e79e675db000879adf1a8ffda44267cfd4aab
taehuuun <lsw463@naver.com>
2023-08-26 02:35:01 +0900

    Remove: 불필요한 파일 제거

--- Assets/01.Scripts/Interface/ISubject.cs
+++
@@ -1,6 +0,0 @@
-public interface ISubject
-{
-    void RegisterObserver(IObserver observer);
-    void RemoveObserver(IObserver observer);
-    void NotifyObservers();
-}
--- Assets/01.Scripts/UI/Page/Page.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class Page : MonoBehaviour
-{
-    public abstract void Open();
-    public abstract void Close();
-    protected abstract void SetupSlots(); 
-}
