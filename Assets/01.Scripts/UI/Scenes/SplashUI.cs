d8285bab6a8d46ac878fc4a98ad2f327d884b099
taehuuun <lsw463@naver.com>
2023-08-28 07:45:09 +0900

    Rename: LoginUI에서 SplashUI로 이름 변경

--- Assets/01.Scripts/UI/Scenes/SplashUI.cs
+++ Assets/01.Scripts/UI/Scenes/SplashUI.cs
@@ -1,7 +1,17 @@
-public class LoginUI : BodyUI
+using System.Collections;
+using UnityEngine;
+
+public class SplashUI : BodyUI
 {
+    [SerializeField] private SplashText splashText;
     private void Start()
     {
+        StartCoroutine(WaitSplash());
+    }
+
+    private IEnumerator WaitSplash()
+    {
+        yield return new WaitUntil(() => splashText.Complete);
         LoadingTaskManager.Instance.AddTask(GPGSHelper.Init, "구글 플레이 서비스 초기화...");
         LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
         LoadingTaskManager.Instance.AddTask(DataSyncManager.SyncUserDataAndLocalData, "유저 / 로컬 데이터 동기화 중...");
