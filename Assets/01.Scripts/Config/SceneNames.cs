385f09dce267ec7ba0bed0b73b0f709f6de4420f
taehuuun <lsw463@naver.com>
2023-08-28 07:46:39 +0900

    Refactor: 미사용 필드 제거

--- Assets/01.Scripts/Config/SceneNames.cs
+++ Assets/01.Scripts/Config/SceneNames.cs
@@ -1,6 +1,5 @@
 public static class SceneNames
 {
-    public const string LoginScene = "00.LoginScene";
     public const string LoadingScene = "01.LoadingScene";
     public const string MainScene = "02.MainScene";
     public const string GalleryScene = "03.GalleryScene";
