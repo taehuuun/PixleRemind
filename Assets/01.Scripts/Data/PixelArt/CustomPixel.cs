f9a347a22641f42ff995bd8f1fba58d0729696e5
taehuuun <lsw463@naver.com>
2023-08-29 23:09:09 +0900

    Fix: Firstore 데이터 로드 시 필요한 기본 생성자 구현

--- Assets/01.Scripts/Data/PixelArt/CustomPixel.cs
+++ Assets/01.Scripts/Data/PixelArt/CustomPixel.cs
@@ -8,7 +8,8 @@ public class CustomPixel
     [FirestoreProperty] public CustomColor OriginalColor { get; private set; }
     [FirestoreProperty] public CustomColor GrayColor { get; private set; }
     [FirestoreProperty] public List<PixelCoord> PixelCoords { private get; set; }
-    
+
+    public CustomPixel() : this(new CustomColor()){}
     public CustomPixel(CustomColor customColor)
     {
         OriginalColor = customColor;
