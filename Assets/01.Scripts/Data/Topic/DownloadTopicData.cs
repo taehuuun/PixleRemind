5eeef9f101ad227a561ded92749bdbbce9a89248
taehuuun <lsw463@naver.com>
2023-08-29 23:12:56 +0900

    Fix: DownloadTopicData 접근 제한자 수정

--- Assets/01.Scripts/Data/Topic/DownloadTopicData.cs
+++ Assets/01.Scripts/Data/Topic/DownloadTopicData.cs
@@ -5,8 +5,8 @@ using Firebase.Firestore;
 [FirestoreData, Serializable]
 public class DownloadTopicData : BaseData
 {
-    [FirestoreProperty] public int TotalPixelArtDataCount { get; private set; }
-    [FirestoreProperty] public List<CollectedPixelArtData> CollectedPixelArtDataList { private get; set; }
+    [FirestoreProperty] public int TotalPixelArtDataCount { get; set; }
+    [FirestoreProperty] public List<CollectedPixelArtData> CollectedPixelArtDataList { get; set; }
 
     public DownloadTopicData() : this(string.Empty,string.Empty, string.Empty,0) { }
     public DownloadTopicData(string id, string title, string description, int totalPixelArtDataCount) : base(id, title, description)
