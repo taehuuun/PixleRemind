ab126e81d2fc2530150f40d5a58f340b6c79b1b8
taehuuun <lsw463@naver.com>
2023-08-29 09:06:40 +0900

    Feat: Loading 씬 내 텍스트 강조 효과 구현

--- Assets/01.Scripts/UI/Scenes/LoadingUI.cs
+++ Assets/01.Scripts/UI/Scenes/LoadingUI.cs
@@ -2,40 +2,71 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using Michsky.MUIP;
 
 public class LoadingUI : BodyUI
 {
-    [SerializeField] private Image loadingBar;
+    [SerializeField] private ProgressBar loadingBar;
+    // [SerializeField] private Image loadingBar;
     [SerializeField] private TMP_Text taskText;
+    [SerializeField] private TMP_Text[] splashTexts;
+    [SerializeField] private float scaleDuration;
+    [SerializeField] private float scaleDelay;
+    [SerializeField] private float repeatDelay;
+    [SerializeField] private string fullText;
 
+    private WaitForSeconds _waitForRepeatDelay;
+    private WaitForSeconds _waitForScaleDelay;
+    
     private void Start()
     {
-        loadingBar.fillAmount = 0f;
+        _waitForRepeatDelay = new WaitForSeconds(repeatDelay);
+        _waitForScaleDelay = new WaitForSeconds(scaleDelay);
+        loadingBar.currentPercent = 0f;
         taskText.text = "";
 
+        StartCoroutine(ScaleText());
         StartCoroutine(ShowLoadingProgress());
         LoadingTaskManager.Instance.LoadSceneAsync(LoadingTaskManager.Instance.NextSceneName);
         _ = LoadingTaskManager.Instance.RunTasks();
+    }
+
+    private IEnumerator ScaleText()
+    {
+        while (true)
+        {
+            for (int i = 0; i < fullText.Length; i++)
+            {
+                StartCoroutine(UIAnimation.ScaleUI(splashTexts[i].transform, splashTexts[i].transform.localScale * 1.1f,scaleDuration));
+                yield return _waitForScaleDelay;
+            }
+
+            yield return _waitForRepeatDelay;
+        }
     }
     
     private IEnumerator ShowLoadingProgress()
     {
         while (!LoadingTaskManager.Instance.AllTaskComplete)
         {
-            loadingBar.fillAmount = LoadingTaskManager.Instance.TaskProgress;
+            loadingBar.currentPercent = LoadingTaskManager.Instance.TaskProgress * 100f;
+            loadingBar.textPercent.text = $"{LoadingTaskManager.Instance.TaskProgress * 100f:#.##} %";
             taskText.text = LoadingTaskManager.Instance.CurrentTask;
 
             yield return null;
         }
 
-        loadingBar.fillAmount = 1f;
+        loadingBar.currentPercent = 100f;
+        loadingBar.textPercent.text = $"100 %";
         taskText.text = $"{LoadingTaskManager.Instance.NextSceneName} 씬으로 로딩중..";
         
         LoadingTaskManager.Instance.ActivateScene();
 
         while (!LoadingTaskManager.Instance.IsSceneLoadingCompleted())
         {
-            loadingBar.fillAmount = LoadingTaskManager.Instance.TaskProgress;
+            loadingBar.currentPercent = LoadingTaskManager.Instance.TaskProgress * 100f;
+            loadingBar.textPercent.text = $"{LoadingTaskManager.Instance.TaskProgress * 100f:#.##} %";
+            
             yield return null;
         }
         
