0e8494189302b8c93594af903e948723f3de2d6f
taehuuun <lsw463@naver.com>
2023-09-04 14:55:41 +0900

    Rename: BodyUI의 클래스명을 BaseSceneUI로 변경

--- Assets/01.Scripts/UI/Base/BaseSceneUI.cs
+++ Assets/01.Scripts/UI/Base/BaseSceneUI.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public abstract class BodyUI : MonoBehaviour
+public abstract class BaseSceneUI : MonoBehaviour
 {
     public void MoveScene(string scene)
     {
