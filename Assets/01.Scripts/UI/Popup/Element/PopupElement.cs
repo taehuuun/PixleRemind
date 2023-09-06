24643c85742aab63406f7958b0eabd688d258aae
taehuuun <lsw463@naver.com>
2023-09-07 07:17:07 +0900

    Feat: 추상 클래스 PopupElement 프로퍼티 및 메서드 구현

--- Assets/01.Scripts/UI/Popup/Element/PopupElement.cs
+++ Assets/01.Scripts/UI/Popup/Element/PopupElement.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
-public class PopupElement : MonoBehaviour
+public abstract class PopupElement : MonoBehaviour
 {
+    public bool IsActive => gameObject.activeSelf;
 
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+    public abstract void ResetElements();
+    public abstract void UpdateElements();
 }
