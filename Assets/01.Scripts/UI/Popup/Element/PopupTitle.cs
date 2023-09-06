8771ae16db371fc469e59c5b06d7490e2f211017
taehuuun <lsw463@naver.com>
2023-09-07 07:28:00 +0900

    Feat: PopupTitle 구현
    PopupElement를 상속 받아 필요 프로퍼티, 메서드 및 추상 메서드 구현

--- Assets/01.Scripts/UI/Popup/Element/PopupTitle.cs
+++ Assets/01.Scripts/UI/Popup/Element/PopupTitle.cs
@@ -1,6 +1,22 @@
+using TMPro;
 using UnityEngine;
 
-public class PopupTitle : MonoBehaviour
+public class PopupTitle : PopupElement
 {
+    [SerializeField] private TMP_Text titleText;
 
+    public void SetTitle(string title)
+    {
+        titleText.text = title;
+    }
+    public void SetTitleStyle(TMP_Style style)
+    {
+        titleText.textStyle = style;
+    }
+    
+    public override void ResetElements()
+    {
+        titleText.text = string.Empty;
+    }
+    public override void UpdateElements() { }
 }
