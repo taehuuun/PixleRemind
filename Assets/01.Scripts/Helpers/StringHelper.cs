fba269c97077d3e8c82abf0d2307aeb14c63daf3
taehuuun <lsw463@naver.com>
2023-09-06 12:55:20 +0900

    Remove: UIHelper 제거 기존 메서드 StringHelper로 이전
    기존 UIHelper 클래스 내 FormatSecondsToTimeString 메서드를 StringHelper로 이전

--- Assets/01.Scripts/Helpers/StringHelper.cs
+++ Assets/01.Scripts/Helpers/StringHelper.cs
@@ -1,4 +1,16 @@
 public static class StringHelper
 {
+    /// <summary>
+    /// 초를 문자열 시분초 형태로 출력 60 -> "01:00"
+    /// </summary>
+    /// <param name="totalSeconds">초 값</param>
+    /// <returns>시분초 형태의 문자열</returns>
+    public static string FormatSecondsToTimeString(int totalSeconds)
+    {
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
 
+        return $"{hours:D2}:{minutes:D2}:{seconds:D2}";
+    }
 }
