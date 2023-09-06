5ef456cfe891538a3dcc3221d4164eaba91de776
taehuuun <lsw463@naver.com>
2023-09-07 07:47:00 +0900

    Feat: PopupContent 구현
    PopupElement를 상속 받아 필요 프로퍼티, 메서드 및 추상 메서드 구현

--- Assets/01.Scripts/UI/Popup/Element/PopupContent.cs
+++ Assets/01.Scripts/UI/Popup/Element/PopupContent.cs
@@ -1,6 +1,22 @@
+using TMPro;
 using UnityEngine;
 
-public class PopupContent : MonoBehaviour
+public class PopupContent : PopupElement
 {
+    [SerializeField] private TMP_Text contentText;
 
+    public void SetContent(string content)
+    {
+        contentText.text = content;
+    }
+    public void SetContentStyle(TMP_Style style)
+    {
+        contentText.textStyle = style;
+    }
+    
+    public override void ResetElements()
+    {
+        contentText.text = string.Empty;
+    }
+    public override void UpdateElements() {}
 }
