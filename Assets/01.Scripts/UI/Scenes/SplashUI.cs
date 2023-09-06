988269436292c3142e283cc565feaccc5460c66c
taehuuun <lsw463@naver.com>
2023-09-06 23:38:35 +0900

    Refactor: LoadingManager 리팩토링으로 인한 LoadingUI 클래스 수정
    - SplashAfter 메서드 수정

--- Assets/01.Scripts/UI/Scenes/SplashUI.cs
+++ Assets/01.Scripts/UI/Scenes/SplashUI.cs
@@ -53,11 +53,10 @@ public class SplashUI : BaseSceneUI
     
     private IEnumerator SplashAfter()
     {
-        LoadingTaskManager.Instance.AddTask(GPGSHelper.Init, "구글 플레이 서비스 초기화...");
-        LoadingTaskManager.Instance.AddTask(LoginManager.Login, "로그인...");
-        LoadingTaskManager.Instance.AddTask(DataSyncManager.SyncUserDataAndLocalData, "유저 / 로컬 데이터 동기화 중...");
-        LoadingTaskManager.Instance.NextSceneName = SceneNames.MainScene;
-        MoveScene(SceneNames.LoadingScene);
+        LoadingManager.Instance.AddTask(GPGSHelper.Init, "구글 플레이 서비스 초기화...");
+        LoadingManager.Instance.AddTask(LoginManager.Login, "로그인...");
+        LoadingManager.Instance.AddTask(DataSyncManager.SyncUserDataAndLocalData, "유저 / 로컬 데이터 동기화 중...");
+        LoadingManager.Instance.StartNextSceneLoading(SceneNames.MainScene);
         yield return null;
     }
 }
