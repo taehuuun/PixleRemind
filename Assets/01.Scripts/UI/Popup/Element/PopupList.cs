8ad7ff4735a522a10a8534ece3591b1f9af00801
taehuuun <lsw463@naver.com>
2023-09-07 10:40:58 +0900

    Feat: PopupList 구현
    PopupElement를 상속 받아 필요 프로퍼티, 메서드 및 추상 메서드 구현

--- Assets/01.Scripts/UI/Popup/Element/PopupList.cs
+++ Assets/01.Scripts/UI/Popup/Element/PopupList.cs
@@ -1,6 +1,32 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PopupList : MonoBehaviour
+public class PopupList : PopupElement
 {
+    [SerializeField] private Transform listContainer;
+    [SerializeField] private GameObject listItemPrefab;
 
+    private List<GameObject> _listItems = new List<GameObject>();
+
+    public void AddItem()
+    {
+        GameObject newItem = Instantiate(listItemPrefab, listContainer);
+        _listItems.Add(newItem);
+    }
+
+    public void ClearList()
+    {
+        foreach (var item in _listItems)
+        {
+            Destroy(item);
+        }
+        
+        _listItems.Clear();
+    }
+    
+    public override void ResetElements()
+    {
+        ClearList();
+    }
+    public override void UpdateElements() { }
 }
