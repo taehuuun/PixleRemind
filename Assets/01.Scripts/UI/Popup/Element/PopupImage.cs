6fc603c83023590a6e3cc6bc55bb9b2a6f8821ae
taehuuun <lsw463@naver.com>
2023-09-07 10:34:31 +0900

    Feat: PopupImage 구현
    PopupElement를 상속 받아 필요 프로퍼티, 메서드 및 추상 메서드 구현

--- Assets/01.Scripts/UI/Popup/Element/PopupImage.cs
+++ Assets/01.Scripts/UI/Popup/Element/PopupImage.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PopupImage : MonoBehaviour
+public class PopupImage : PopupElement
 {
+    [SerializeField] private Image popupImage;
 
+    public void SetImage(Sprite sprite)
+    {
+        popupImage.sprite = sprite;
+    }
+    public void SetImageColor(Color color)
+    {
+        popupImage.color = color;
+    }
+    
+    public override void ResetElements()
+    {
+        popupImage.sprite = null;
+    }
+    public override void UpdateElements() { }
 }
